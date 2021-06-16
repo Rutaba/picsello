4eaa5aae17cdd7be66072663a6156b5ae300a1f4
Gabriel Reis <gabriel@hashrocket.com>
2021-06-16 17:11:52 -0500

    style home page text

---
+++ test/features/welcome_page_test.exs
@@ -0,0 +1,15 @@
+defmodule Picsello.WelcomePageTest do
+  use Picsello.FeatureCase
+  import Picsello.AccountsFixtures
+
+  feature "user logs in", %{session: session} do
+    user = user_fixture(%{first_name: "Morty", last_name: "Smith"})
+
+    session
+    |> sign_in(user)
+    |> assert_has(css("h1", text: "Hello Morty"))
+    |> assert_has(css("header", text: "MS"))
+
+    assert current_path(session) == "/home"
+  end
+end
