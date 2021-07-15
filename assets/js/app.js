bc5b453ea255171dc0725bfb8b899cc4513753b8
Brian Dunn <brian@hashrocket.com>
2021-07-15 15:57:29 -0500

    Fix background scrolldal in ios
    [#178884838]

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,8 +22,19 @@ import "@fontsource/be-vietnam/400.css"
 import "@fontsource/be-vietnam/500.css"
 import "@fontsource/be-vietnam/700.css"
 
+let Hooks = {}
+
+Hooks.Modal = {
+  mounted() {
+    document.body.style.overflow = "hidden"
+  },
+  destroyed() {
+    document.body.style.overflow = ""
+  }
+}
+
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let liveSocket = new LiveSocket("/live", Socket, {hooks: Hooks, params: {_csrf_token: csrfToken}})
 
 // Show progress bar on live navigation and form submits
 topbar.config({barColors: {0: "#00ADC9"}, shadowColor: "rgba(0, 0, 0, .3)"})
