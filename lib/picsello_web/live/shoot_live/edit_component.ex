9aef11f53e661d16f4717c9f982cdfb6abaf5ee1
Brian Dunn <brian@hashrocket.com>
2021-09-28 13:16:32 -0500

    Convert edit shoot to heex

--- lib/picsello_web/live/shoot_live/edit_component.ex
+++ lib/picsello_web/live/shoot_live/edit_component.ex
@@ -13,6 +13,32 @@ defmodule PicselloWeb.ShootLive.EditComponent do
   end
 
   @impl true
+  def render(assigns) do
+    ~H"""
+      <div class="flex flex-col modal">
+        <h2 class="text-xl font-semibold"><%= Job.name @job %></h2>
+        <h1 class="mt-2 title">Edit Shoot</h1>
+
+        <.form let={f} for={@changeset}, phx-change="validate" phx-submit="save" phx-target={@myself}>
+          <%= labeled_input f, :name, label: "Shoot name", placeholder: "Engagement Shoot" %>
+          <%= labeled_input f, :starts_at, type: :datetime_local_input, label: "Shoot date", min: Date.utc_today(), time_zone: @current_user.time_zone, wrapper_class: "mt-4" %>
+          <%= labeled_select f, :duration_minutes, for(duration <- Shoot.durations(), do: {dyn_gettext("duration-#{duration}"), duration }), label: "Shoot duration", prompt: "Select below", wrapper_class: "mt-4" %>
+          <div class="mt-4 input-label">Shoot location</div>
+          <%= for location <- Shoot.locations() do %>
+            <label class="flex items-center mb-2">
+              <%= radio_button f, :location, location, class: "mr-2 radio" %>
+              <%= dyn_gettext location %>
+            </label>
+          <% end %>
+          <%= labeled_input f, :notes, type: :textarea, label: "Shoot notes", placeholder: "type notes here" %>
+
+          <%= live_component PicselloWeb.LiveModal.FooterComponent, disabled: !@changeset.valid? %>
+        </.form>
+      </div>
+    """
+  end
+
+  @impl true
   def handle_event("validate", %{"shoot" => params}, socket) do
     socket |> assign_changeset(params, :validate) |> noreply()
   end
