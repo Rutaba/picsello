e9ce1f474122cbdde0120c3d84a7fff6dac525cb
sviatosluv <sviatoslav.flud@litslink.com>
2021-10-08 19:24:16 +0300

    add docs to the Galleries context functions

--- lib/picsello/galleries.ex
+++ lib/picsello/galleries.ex
@@ -37,6 +37,21 @@ defmodule Picsello.Galleries do
   """
   def get_gallery!(id), do: Repo.get!(Gallery, id)
 
+
+  @doc """
+  Gets a single gallery by hash parameter.
+
+  Returns nil if the Gallery does not exist.
+
+  ## Examples
+
+      iex> get_gallery_by_hash("validhash")
+      %Gallery{}
+
+      iex> get_gallery!("wronghash")
+      nil
+
+  """
   def get_gallery_by_hash(hash) do
     Repo.get_by(Gallery, client_link_hash: hash)
   end
@@ -105,17 +120,53 @@ defmodule Picsello.Galleries do
   def change_gallery(%Gallery{} = gallery, attrs \\ %{}) do
     Gallery.update_changeset(gallery, attrs)
   end
+  
+  @doc """
+  Creates a photo.
 
+  ## Examples
+
+      iex> create_photo(%{field: value})
+      {:ok, %Photo{}}
+
+      iex> create_photo(%{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
   def create_photo(%{} = attrs) do
     attrs
     |> Photo.create_changeset()
     |> Repo.insert()
   end
 
-  @doc false
+  @doc """
+  Gets a single photo by id.
+
+  Returns nil if the Photo does not exist.
+
+  ## Examples
+
+      iex> get_photo(123)
+      %Photo{}
+
+      iex> get_photo(44545)
+      nil
+
+  """
   def get_photo(id), do: Repo.get(Photo, id)
 
-  @doc false
+  @doc """
+  Marks a photo as liked/unliked.
+
+  ## Examples
+
+      iex> mark_photo_as_liked(%Photo{client_liked: false})
+      {:ok, %Photo{client_liked: true}}
+
+      iex> mark_photo_as_liked(%Photo{client_liked: true})
+      {:ok, %Photo{client_liked: false}}
+
+  """
   def mark_photo_as_liked(%Photo{client_liked: client_liked} = photo) do
     photo
     |> Photo.update_changeset(%{client_liked: !client_liked})
