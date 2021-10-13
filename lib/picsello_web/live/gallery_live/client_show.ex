932040b18b1dbc18bbe94486a6b74c025a192108
sviatosluv <sviatoslav.flud@litslink.com>
2021-10-13 17:43:34 +0300

    implement favorites filter to the gallery photos

--- lib/picsello_web/live/gallery_live/client_show.ex
+++ lib/picsello_web/live/gallery_live/client_show.ex
@@ -14,13 +14,15 @@ defmodule PicselloWeb.GalleryLive.ClientShow do
   @impl true
   def handle_params(%{"hash" => hash}, _, socket) do
     gallery = Galleries.get_detailed_gallery_by_hash(hash)
-
+    
     if gallery do
       socket
       |> assign(:hash, hash)
       |> assign(:gallery, gallery)
       |> assign(:page_title, "Show Gallery")
       |> assign(:page, 0)
+      |> assign(:update_mode, "append")
+      |> assign(:favorites_filter, false)
       |> assign_photos()
       |> noreply()
     else
@@ -31,11 +33,21 @@ defmodule PicselloWeb.GalleryLive.ClientShow do
   def handle_event("load-more", _, %{assigns: %{page: page}} = socket) do
     socket
     |> assign(page: page + 1)
+    |> assign(:update_mode, "append")
+    |> assign_photos()
+    |> noreply()
+  end
+  
+  def handle_event("toggle_favorites", _, %{assigns: %{favorites_filter: toggle_state}} = socket) do
+    socket
+    |> assign(:page, 0)
+    |> assign(:update_mode, "replace")
+    |> assign(:favorites_filter, !toggle_state)
     |> assign_photos()
     |> noreply()
   end
 
-  defp assign_photos(%{assigns: %{gallery: %{id: id}, page: page}} = socket) do
-    assign(socket, photos: Galleries.get_gallery_photos(id, @per_page, page))
+  defp assign_photos(%{assigns: %{gallery: %{id: id}, page: page, favorites_filter: filter}} = socket) do
+    assign(socket, photos: Galleries.get_gallery_photos(id, @per_page, page, only_favorites: filter))
   end
 end
