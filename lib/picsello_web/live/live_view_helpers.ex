ebf5a9d68b3699a84757f7255253a79ee5cc50ac
Brian Dunn <brian@hashrocket.com>
2021-10-26 14:55:50 -0500

    Upgrade liveview

--- lib/picsello_web/live/live_view_helpers.ex
+++ lib/picsello_web/live/live_view_helpers.ex
@@ -4,7 +4,7 @@ defmodule PicselloWeb.LiveViewHelpers do
   alias PicselloWeb.LiveAuth
 
   def assign_defaults(socket, session) do
-    case LiveAuth.mount(%{}, session, socket) do
+    case LiveAuth.on_mount(:default, %{}, session, socket) do
       {:cont, socket} -> socket
       {:halt, socket} -> socket |> assign(:current_user, nil)
     end
