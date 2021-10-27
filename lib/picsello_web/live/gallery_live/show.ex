759c24996e3f1673ee8d8cb03c15ff11ef6a0a09
sviatosluv <sviatoslav.flud@litslink.com>
2021-10-27 09:15:15 +0300

    upload photos in modal window

--- lib/picsello_web/live/gallery_live/show.ex
+++ lib/picsello_web/live/gallery_live/show.ex
@@ -3,31 +3,13 @@ defmodule PicselloWeb.GalleryLive.Show do
   use PicselloWeb, live_view: [layout: "live_client"]
 
   alias Picsello.Galleries
+  alias PicselloWeb.GalleryLive.UploadComponent
 
   @per_page 12
-  @upload_options [
-    accept: ~w(.jpg .jpeg .png),
-    max_entries: 50,
-    max_file_size: 104_857_600,
-    auto_upload: true
-  ]
-  @bucket "picsello-staging"
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok,
-     socket
-     |> assign(:uploaded_files, [])
-     |> assign(:gcp_credentials, gcp_credentials())
-     |> assign(:upload_bucket, @bucket)
-     |> assign(:overall_progress, 0)
-     |> allow_upload(
-       :photo,
-       Keyword.merge(@upload_options,
-         external: &presign_entry/2,
-         progress: &handle_progress/3
-       )
-     )}
+    {:ok, socket}
   end
 
   @impl true
@@ -42,6 +24,12 @@ defmodule PicselloWeb.GalleryLive.Show do
     |> assign(:favorites_filter, false)
     |> assign(:favorites_count, Galleries.gallery_favorites_count(gallery))
     |> assign_photos()
+    |> noreply()
+  end
+
+  def handle_event("upload_popup", _, socket) do
+    socket
+    |> open_modal(UploadComponent, %{index: "hello", id: 13777})
     |> noreply()
   end
 
@@ -63,58 +51,13 @@ defmodule PicselloWeb.GalleryLive.Show do
     |> noreply()
   end
 
-  @impl true
-  def handle_event("start", _params, socket) do
-    send(self(), :overall_progress)
+  def handle_info({:overall_progress, upload_state}, socket) do
+    IO.inspect upload_state
+    send_update(self(), UploadComponent, id: "hello", overall_progress: 1)
+    
     {:noreply, socket}
   end
-
-  def handle_info(:overall_progress, socket) do
-    uploading_achievements = for entry <- socket.assigns.uploads.photo.entries, do: entry.progress
-    overall_progress = Enum.sum(uploading_achievements) / Enum.count(uploading_achievements)
-
-    unless done?(overall_progress) do
-      Process.send_after(self(), :overall_progress, 500)
-    end
-
-    {:noreply, assign(socket, :overall_progress, overall_progress)}
-  end
-
-  @impl true
-  def handle_event("cancel-upload", %{"ref" => ref}, socket) do
-    {:noreply, cancel_upload(socket, :photo, ref)}
-  end
-
-  defp handle_progress(:photo, entry, %{assigns: assigns} = socket) do
-    if entry.done? do
-      sign_opts = [bucket: assigns.upload_bucket, key: entry.client_name]
-      upload = GCSSign.sign_url_v4(socket.assigns.gcp_credentials, sign_opts)
-
-      {:noreply, update(socket, :uploaded_files, &(&1 ++ [upload]))}
-    else
-      {:noreply, socket}
-    end
-  end
-
-  defp presign_entry(entry, socket) do
-    key = entry.client_name
-    sign_opts = [
-      expires_in: 600,
-      bucket: socket.assigns.upload_bucket,
-      key: key,
-      fields: %{
-        "content-type" => entry.client_type,
-        "cache-control" => "public, max-age=@upload_options"
-      },
-      conditions: [["content-length-range", 0, 104_857_600]]
-    ]
-
-    {:ok, params} = GCSSign.sign_post_policy_v4(socket.assigns.gcp_credentials, sign_opts)
-    meta = %{uploader: "GCS", key: key, url: params[:url], fields: params[:fields]}
-
-    {:ok, meta, socket}
-  end
-
+  
   defp assign_photos(
          %{
            assigns: %{
@@ -131,15 +74,4 @@ defmodule PicselloWeb.GalleryLive.Show do
 
   defp page_title(:show), do: "Show Gallery"
   defp page_title(:edit), do: "Edit Gallery"
-
-  defp total(list) when is_list(list), do: list |> length
-  defp total(_), do: nil
-
-  defp done?(progress), do: progress == 100
-
-  defp gcp_credentials do
-    Application.get_env(:gcs_sign, :gcp_credentials)
-    |> File.read!()
-    |> Jason.decode!()
-  end
 end
