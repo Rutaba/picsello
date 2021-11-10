4e23cd447f3d395e8eed4d7f3d8000436c26648c
Brian Dunn <brian@hashrocket.com>
2021-11-10 09:25:14 -0600

    use expires from token req

--- lib/picsello/whcc/client.ex
+++ lib/picsello/whcc/client.ex
@@ -1,55 +1,67 @@
 defmodule Picsello.WHCC.Client do
   use Tesla
-  plug Tesla.Middleware.JSON
-  plug Tesla.Middleware.BaseUrl, config() |> Keyword.get(:url)
-  plug Tesla.Middleware.Logger
+  alias Picsello.WHCC.Category
+  plug(Tesla.Middleware.JSON)
+  plug(Tesla.Middleware.BaseUrl, config() |> Keyword.get(:url))
+  plug(Tesla.Middleware.Logger)
+
+  @moduledoc "client for whcc http api"
+  @behaviour Picsello.WHCC.Adapter
 
   defmodule TokenStore do
     use Agent
+    @moduledoc false
 
-    def start_link(_) do
-      Agent.start_link(fn -> %{} end, name: __MODULE__)
-    end
+    def start_link(_), do: Agent.start_link(fn -> %{} end, name: __MODULE__)
 
     def get_and_update(f), do: Agent.get_and_update(__MODULE__, f)
   end
 
   def token() do
-    TokenStore.get_and_update(fn state ->
-      %{token: token} =
-        state =
-        case state do
-          %{fetched_at: fetched_at} = state ->
-            if expired?(fetched_at) do
-              fetch_token()
-            else
-              state
-            end
-
-          %{} ->
+    update =
+      &case &1 do
+        %{fetched_at: fetched_at} ->
+          if expired?(fetched_at) do
             fetch_token()
-        end
+          else
+            &1
+          end
 
+        %{} ->
+          fetch_token()
+      end
+
+    TokenStore.get_and_update(fn state ->
+      %{token: token} = state = update.(state)
       {token, state}
     end)
   end
 
   def products() do
-    new() |> get("/products")
+    {:ok, %{body: body}} = new() |> get("/products")
+    body
   end
 
-  defp new() do
+  @impl true
+  def categories() do
+    products()
+    |> Enum.map(&Map.get(&1, "category"))
+    |> Enum.uniq()
+    |> Enum.map(&%Category{id: &1["id"], name: &1["name"]})
+  end
+
+  def new() do
     Tesla.client([{Tesla.Middleware.BearerAuth, token: token()}])
   end
 
   defp fetch_token() do
-    {:ok, %{body: %{"accessToken" => token}} = request} =
+    {:ok, %{body: %{"accessToken" => token, "expires" => fetched_at}}} =
       post(
         "/auth/access-token",
         config() |> Keyword.take([:key, :secret]) |> Enum.into(%{})
       )
 
-    %{token: token, fetched_at: request_start(request)}
+    %{token: token, fetched_at: DateTime.from_unix!(fetched_at)}
   end
 
   defp expired?(fetched_at) do
