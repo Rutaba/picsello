31f462ff63d9c96fe637a39d2bcfe6b8072c1e70
Brian Dunn <brian@hashrocket.com>
2021-11-10 09:27:31 -0600

    implement category list

--- lib/picsello_web/live/pricing.ex
+++ lib/picsello_web/live/pricing.ex
@@ -2,12 +2,10 @@ defmodule PicselloWeb.Live.Pricing do
   @moduledoc false
   use PicselloWeb, :live_view
   import PicselloWeb.Live.User.Settings, only: [settings_nav: 1]
-  import PicselloWeb.PackageLive.Shared, only: [package_card: 1]
-  alias Picsello.{Package, Repo}
 
   @impl true
   def mount(_params, _session, socket) do
-    socket |> assign_templates() |> ok()
+    socket |> assign_categories() |> ok()
   end
 
   @impl true
@@ -24,14 +22,14 @@ defmodule PicselloWeb.Live.Pricing do
 
       <hr class="mb-7"/>
 
-      <ul class="grid grid-cols-1 sm:grid-cols-4 gap-8">
-      <%= for label <- ["Prints", "Geeting Cards", "Framed Prints", "Custom Albums", "Books"] do %>
-        <li class="border rounded p-5">
-          <div class="aspect-w-1 aspect-h-1 w-full rounded bg-base-200">
-            <.icon name="prints" class="text-blue-planning-300" />
+      <ul class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-8">
+      <%= for %{name: name, icon: icon} <- @categories do %>
+        <li class="p-5 border rounded">
+          <div class="w-full rounded aspect-w-1 aspect-h-1 bg-base-200">
+            <.icon name={icon} class="text-blue-planning-300" />
           </div>
 
-          <h2 class="text-2xl font-bold pt-3"><%= label %></h2>
+          <h2 class="pt-3 text-2xl font-bold"><%= name %></h2>
         </li>
       <% end %>
       </ul>
@@ -39,76 +37,7 @@ defmodule PicselloWeb.Live.Pricing do
     """
   end
 
-  @impl true
-  def handle_event("add-package", %{}, %{assigns: assigns} = socket),
-    do:
-      socket
-      |> open_modal(PicselloWeb.PackageLive.WizardComponent, assigns |> Map.take([:current_user]))
-      |> noreply()
-
-  @impl true
-  def handle_event(
-        "edit-package",
-        %{"package-id" => package_id},
-        %{assigns: %{current_user: current_user, templates: templates}} = socket
-      ) do
-    package_id = String.to_integer(package_id)
-
-    package = Enum.find(templates, &(&1.id == package_id))
-
-    socket
-    |> open_modal(PicselloWeb.PackageLive.WizardComponent, %{
-      current_user: current_user,
-      package: package
-    })
-    |> noreply()
-  end
-
-  @impl true
-  def handle_event("confirm-archive-package", %{"package-id" => package_id}, socket),
-    do:
-      socket
-      |> assign(:archive_package_id, package_id)
-      |> PicselloWeb.ConfirmationComponent.open(%{
-        close_label: "No! Get me out of here",
-        confirm_event: "archive",
-        confirm_label: "Yes, archive",
-        icon: "warning-orange",
-        subtitle:
-          "Archiving a package template doesn’t affect active leads or jobs—this will remove the option to create anything with this package template.",
-        title: "Are you sure you want to archive this package template?"
-      })
-      |> noreply()
-
-  @impl true
-  def handle_info(
-        {:confirm_event, "archive"},
-        %{assigns: %{archive_package_id: package_id}} = socket
-      ) do
-    with %Package{} = package <- Repo.get(Package, package_id),
-         {:ok, _package} <- package |> Package.archive_changeset() |> Repo.update() do
-      socket
-      |> assign_templates()
-      |> put_flash(:success, "The package has been archived")
-      |> close_modal()
-      |> noreply()
-    else
-      _ ->
-        socket
-        |> put_flash(:error, "Failed to archive package")
-        |> noreply()
-    end
-  end
-
-  @impl true
-  def handle_info({:update, _package}, socket) do
-    socket
-    |> assign_templates()
-    |> put_flash(:success, "The package has been successfully saved")
-    |> noreply()
-  end
-
-  defp assign_templates(%{assigns: %{current_user: user}} = socket) do
-    socket |> assign(templates: user |> Package.templates_for_user() |> Repo.all())
+  defp assign_categories(socket) do
+    socket |> assign(categories: Picsello.WHCC.categories())
   end
 end
