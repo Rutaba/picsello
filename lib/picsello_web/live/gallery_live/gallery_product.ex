77eb9c54ce3784167daa6b2180f5921c3d738413
Chernov Igor <sinnhro@gmail.com>
2021-12-02 10:18:11 +0200

    Changes in gallery product

--- lib/picsello_web/live/gallery_live/gallery_product.ex
+++ lib/picsello_web/live/gallery_live/gallery_product.ex
@@ -36,6 +36,11 @@ defmodule PicselloWeb.GalleryLive.GalleryProduct do
       {:ok, redirect(socket, to: "/")}
     else
       url = preview.preview_photo.preview_url || nil
+<<<<<<< HEAD
+=======
+      frame = preview.category_template.name || nil
+      frame_corners = preview.category_template.corners || nil
+>>>>>>> e8d6bc5 (added frames for gallery preview)
 
       {:ok,
        socket
