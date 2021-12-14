6ac53727b281be4696255a2febfeabc34a6504e7
Serhii Lukianov <serhii.lukianov@litslink.com>
2021-12-14 12:34:54 +0200

    GAL-77 editor details

--- lib/picsello/whcc/editor/details.ex
+++ lib/picsello/whcc/editor/details.ex
@@ -1,5 +1,13 @@
 defmodule Picsello.WHCC.Editor.Details do
+  @moduledoc "Editor detais structure to be used in cart"
   defstruct [:product_id, :editor_id, :preview_url, :selections]
+
+  @type t :: %__MODULE__{
+          editor_id: String.t(),
+          product_id: String.t(),
+          preview_url: String.t(),
+          selections: %{}
+        }
 
   def new(%{
         "_id" => editor_id,
@@ -10,8 +18,7 @@ defmodule Picsello.WHCC.Editor.Details do
     url =
       preview
       |> Map.to_list()
-      |> Enum.at(0)
-      |> then(fn {_, x} -> x end)
+      |> then(fn [{_, x} | _] -> x end)
       |> Map.get("scale_1024")
 
     %__MODULE__{
