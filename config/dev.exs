6211366e951979a5f4d254ba1527a4f44055db4c
Brian Dunn <brian@hashrocket.com>
2021-12-16 17:02:33 -0600

    reload on lib changes too

--- config/dev.exs
+++ config/dev.exs
@@ -61,7 +61,8 @@ config :picsello, PicselloWeb.Endpoint,
       ~r"priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$",
       ~r"priv/gettext/.*(po)$",
       ~r"lib/picsello_web/(live|views)/.*(ex)$",
-      ~r"lib/picsello_web/templates/.*(eex)$"
+      ~r"lib/picsello_web/templates/.*(eex)$",
+      ~r"lib/picsello/.*(ex)$"
     ]
   ]
 
