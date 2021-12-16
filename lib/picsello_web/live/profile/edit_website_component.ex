b4ddf19aa6f251a77635752e4fe45c36d4613a4a
Gabriel Reis <gabriel.oreis@gmail.com>
2021-12-16 11:19:24 -0300

    Extract Shared module for profile edit components

--- lib/picsello_web/live/profile/edit_website_component.ex
+++ lib/picsello_web/live/profile/edit_website_component.ex
@@ -1,16 +1,6 @@
 defmodule PicselloWeb.Live.Profile.EditWebsiteComponent do
   @moduledoc false
   use PicselloWeb, :live_component
-  alias Picsello.{Profiles}
-
-  @impl true
-  def update(assigns, socket) do
-    socket
-    |> assign(assigns)
-    |> assign_organization()
-    |> assign_changeset()
-    |> ok()
-  end
 
   @impl true
   def render(assigns) do
@@ -24,66 +14,17 @@ defmodule PicselloWeb.Live.Profile.EditWebsiteComponent do
           <.website_field form={p} class="mt-4" placeholder="Add your website…" />
         <% end %>
 
-        <PicselloWeb.LiveModal.footer>
-          <button class="btn-primary" title="save" type="submit" phx-disable-with="Save">
-            Save
-          </button>
-
-          <button class="btn-secondary" title="cancel" type="button" phx-click="modal" phx-value-action="close">
-            Cancel
-          </button>
-        </PicselloWeb.LiveModal.footer>
+        <PicselloWeb.LiveModal.footer />
       </.form>
     </div>
     """
   end
 
   @impl true
-  def handle_event("validate", %{"organization" => params}, socket) do
-    socket |> assign_changeset(params) |> noreply()
-  end
+  defdelegate update(assigns, socket), to: PicselloWeb.Live.Profile.Shared
 
   @impl true
-  def handle_event(
-        "save",
-        %{"organization" => params},
-        %{assigns: %{organization: organization}} = socket
-      ) do
-    case Profiles.update_organization_profile(organization, params) do
-      {:ok, organization} ->
-        send(socket.parent_pid, {:update, organization})
-        socket |> close_modal() |> noreply()
+  defdelegate handle_event(name, params, socket), to: PicselloWeb.Live.Profile.Shared
 
-      {:error, _} ->
-        socket |> noreply()
-    end
-  end
-
-  def open(%{assigns: assigns} = socket, opts \\ %{}),
-    do:
-      open_modal(
-        socket,
-        __MODULE__,
-        %{
-          assigns: Enum.into(opts, Map.take(assigns, [:current_user]))
-        }
-      )
-
-  defp assign_changeset(
-         %{assigns: %{organization: organization}} = socket,
-         params \\ %{},
-         action \\ :validate
-       ) do
-    changeset =
-      organization
-      |> Profiles.edit_organization_profile_changeset(params)
-      |> Map.put(:action, action)
-
-    assign(socket, changeset: changeset)
-  end
-
-  defp assign_organization(%{assigns: %{current_user: current_user}} = socket) do
-    organization = Profiles.find_organization_by(user: current_user)
-    socket |> assign(:organization, organization)
-  end
+  def open(socket), do: PicselloWeb.Live.Profile.Shared.open(socket, __MODULE__)
 end
