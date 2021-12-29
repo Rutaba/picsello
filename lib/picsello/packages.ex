1d731d480378b310a1d2262ddfbf9fc7290b9c43
Brian Dunn <brian@hashrocket.com>
2021-12-29 16:42:27 -0600

    Load calculator data, calculate initial packages
    https://picsello.atlassian.net/browse/PIC-397

--- lib/picsello/packages.ex
+++ lib/picsello/packages.ex
@@ -1,6 +1,16 @@
 defmodule Picsello.Packages do
   @moduledoc "context module for packages"
-  alias Picsello.{Package, Repo, Job, JobType}
+  alias Picsello.{
+    Accounts.User,
+    Package,
+    Repo,
+    Job,
+    JobType,
+    Packages.BasePrice,
+    Packages.CostOfLivingAdjustment
+  }
+
+  import Ecto.Query, only: [from: 2]
 
   defmodule Multiplier do
     @moduledoc false
@@ -177,4 +187,65 @@ defmodule Picsello.Packages do
          %{sign: "-", is_enabled: true, percent: percent} -> percent
          _ -> nil
        end)
+
+  defmacro array_to_string(array, delimiter) do
+    quote do
+      fragment("array_to_string(?, ?)", unquote(array), unquote(delimiter))
+    end
+  end
+
+  defmacro now() do
+    quote do
+      fragment("now()")
+    end
+  end
+
+  def create_initial(
+        %User{
+          onboarding: %{photographer_years: years_experience, schedule: schedule, state: state}
+        } = user
+      )
+      when is_integer(years_experience) and is_atom(schedule) and is_binary(state) do
+    full_time = schedule == :full_time
+
+    %{organization: %{id: organization_id, profile: %{job_types: [_ | _] = job_types}}} =
+      Repo.preload(user, :organization)
+
+    default_each_price = Download.default_each_price()
+
+    download_count = 0
+    shoot_count = 1
+
+    min_years_query =
+      from(base in BasePrice,
+        select: max(base.min_years_experience),
+        where: base.min_years_experience <= ^years_experience
+      )
+
+    templates_query =
+      from(base in BasePrice,
+        where:
+          base.full_time == ^full_time and base.job_type in ^job_types and
+            base.min_years_experience in subquery(min_years_query),
+        join: adjustment in CostOfLivingAdjustment,
+        on: adjustment.state == ^state,
+        select: %{
+          base_price: type(adjustment.multiplier * base.base_price, base.base_price),
+          description: array_to_string([base.tier, base.job_type], " "),
+          download_count: type(^download_count, :integer),
+          download_each_price: type(^default_each_price, base.base_price),
+          inserted_at: now(),
+          job_type: base.job_type,
+          name: array_to_string([base.tier, base.job_type], " "),
+          organization_id: type(^organization_id, base.id),
+          shoot_count: type(^shoot_count, :integer),
+          updated_at: now()
+        }
+      )
+
+    {_count, templates} = Repo.insert_all(Package, templates_query, returning: true)
+    templates
+  end
+
+  def create_initial(_user), do: []
 end
