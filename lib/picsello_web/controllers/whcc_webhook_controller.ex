f675c6fe56ebcefcdebf3a24c98d4b579001ab50
Serhii Lukianov <serhii.lukianov@litslink.com>
2021-12-31 11:54:21 +0200

    GAL-84: webhooks

--- lib/picsello_web/controllers/whcc_webhook_controller.ex
+++ lib/picsello_web/controllers/whcc_webhook_controller.ex
@@ -15,15 +15,34 @@ defmodule PicselloWeb.WhccWebhookController do
           "Status" => "Accepted"
         } = params
       ) do
-    # signature = conn |> get_req_header("whcc-signature")
-    # payload = Jason.encode!(params)
-
-    # WHCC.webhook_validate(payload, signature)
-
-    Picsello.Cart.store_car_product_processing(params)
-
+    Picsello.Cart.store_cart_product_processing(params)
     conn.private.raw_body |> IO.inspect(label: "raw body")
 
+    conn |> ok()
+  end
+
+  def webhook(
+        %Plug.Conn{} = conn,
+        %{
+          "Event" => "Processed",
+          "Status" => "Rejected",
+          "Errors" => errors,
+          "EntryId" => entry
+        } = params
+      ) do
+    Picsello.Cart.store_cart_product_processing(params)
+    Logger.error("[whcc] Error processing #{entry}: #{inspect(errors)}")
+
+    conn |> ok()
+  end
+
+  def webhook(
+        %Plug.Conn{} = conn,
+        %{
+          "Event" => "Shipped"
+        } = params
+      ) do
+    Picsello.Cart.store_cart_product_tracking(params)
     conn |> ok()
   end
 
