b639fb1898225496892481bb6e0d65dd0be9e911
sviatosluv <sviatoslav.flud@litslink.com>
2022-01-03 17:49:29 +0200

    pull whhc product name for physical cart products

--- lib/picsello_web/live/gallery_live/client_show/cart/product.ex
+++ lib/picsello_web/live/gallery_live/client_show/cart/product.ex
@@ -1,6 +1,17 @@
 defmodule PicselloWeb.GalleryLive.ClientShow.Cart.Product do
   @moduledoc false
   use PicselloWeb, :live_component
+  alias Picsello.GalleryProducts
+
+  def update(%{product: cart_product} = assigns, socket) do
+    socket
+    |> assign(assigns)
+    |> assign(
+      :whcc_product,
+      GalleryProducts.get_whcc_product(cart_product.editor_details["product_id"])
+    )
+    |> ok()
+  end
 
   defp product_size(%{editor_details: %{"selections" => %{"size" => size}}}), do: size
   defp product_preview_url(%{editor_details: %{"preview_url" => url}}), do: url
