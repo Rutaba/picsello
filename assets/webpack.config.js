07360f561c74772c6f05053f31b7b08a51188dd6
Kyle Bacon <hello@kylebaconcreative.com>
2022-01-03 21:17:45 -0800

    Add prettierrc and fix file

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -13,21 +13,21 @@ module.exports = (env, options) => {
     optimization: {
       minimizer: [
         new TerserPlugin({ cache: true, parallel: true, sourceMap: devMode }),
-        new OptimizeCSSAssetsPlugin({}),
-      ],
+        new OptimizeCSSAssetsPlugin({})
+      ]
     },
     entry: {
-      app: glob.sync('./vendor/**/*.js').concat(['./js/app.js']),
+      app: glob.sync('./vendor/**/*.js').concat(['./js/app.js'])
     },
     output: {
       filename: '[name].js',
       path: path.resolve(__dirname, '../priv/static/js'),
-      publicPath: '/js/',
+      publicPath: '/js/'
     },
     resolve: {
       alias: {
-        introJs: path.resolve(__dirname, './node_modules/intro.js'),
-      },
+        introJs: path.resolve(__dirname, './node_modules/intro.js')
+      }
     },
     devtool: devMode ? 'eval-cheap-module-source-map' : undefined,
     module: {
@@ -36,8 +36,8 @@ module.exports = (env, options) => {
           test: /\.js$/,
           exclude: /node_modules/,
           use: {
-            loader: 'babel-loader',
-          },
+            loader: 'babel-loader'
+          }
         },
         {
           test: /\.[s]?css$/,
@@ -45,8 +45,8 @@ module.exports = (env, options) => {
             MiniCssExtractPlugin.loader,
             'css-loader',
             'postcss-loader',
-            'sass-loader',
-          ],
+            'sass-loader'
+          ]
         },
         {
           test: /\.(eot|woff|woff2|ttf)$/,
@@ -56,16 +56,16 @@ module.exports = (env, options) => {
               options: {
                 name: '[name].[ext]',
                 path: path.resolve(__dirname, '../priv/static/js'),
-                publicPath: '/js/',
-              },
-            },
-          ],
-        },
-      ],
+                publicPath: '/js/'
+              }
+            }
+          ]
+        }
+      ]
     },
     plugins: [
       new MiniCssExtractPlugin({ filename: '../css/app.css' }),
-      new CopyWebpackPlugin([{ from: 'static/', to: '../' }]),
-    ],
+      new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
+    ]
   };
 };
