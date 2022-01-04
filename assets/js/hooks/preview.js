63b0b729ab04c6a6b8e68ab1103f5c26ed8edc2a
Iryna Obertynska <iryna.obertynska@litslink.com>
2022-01-04 18:40:35 +0200

    fixed mobile styles for client gallery( general alignment and product preview items), refactoring clientGallery.css, fixing styles for image in editProduct popup, updated preview hook

--- assets/js/hooks/preview.js
+++ assets/js/hooks/preview.js
@@ -1,52 +1,60 @@
-var Preview = {
-  mounted() { 
-    this.handleEvent("set_preview", 
-    ({preview: preview_name, frame: frame_name, coords: corners0, target: canvasId}) => {
-      this.draw(frame_name, preview_name, corners0, canvasId);
-    })
+const Preview = {
+    mounted() {
+        this.handleEvent("set_preview",
+            ({preview: preview_name, frame: frame_name, coords: corners0, target: canvasId}) => {
+                this.draw(frame_name, preview_name, corners0, canvasId);
+            })
+    },
 
-  },
-  
-  draw: function(frame_name, preview_name, coord, canvasId) {
-    if(typeof(coord) == 'string'){coord = JSON.parse(coord)};
-    
-    let canvas = document.getElementById(canvasId);
-    if (canvas.getContext) {
-      let ctx = canvas.getContext("2d");
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-      let frame = new Image();
-      frame.src = "/images/" + frame_name;
-
-      let cw = canvas.width;
-      let ch = canvas.height;
-
-      frame.onload = function(){
-        let frameW = frame.width;
-        let frameH = frame.height;
-        let kfw = coord[0]/frameW;
-        let kfh = coord[1]/frameH;
-        let w = coord[6] - coord[0] + 1;
-        let h = coord[7] - coord[1] + 1;
-
-        let kw = cw / frameW;
-        let kh = ch / frameH;
-
-        let preview = new Image();
-        preview.src = preview_name;
-        preview.onload = function(){
-          let width = (w * kw) < 10 && cw || (w * kw);
-          let height = (h * kh) < 10 && ch || (h * kh);
-
-          let lty = ch * kfh;
-          let ltx = cw * kfw;
-
-          ctx.drawImage(frame, 0, 0, cw, ch);
-          ctx.drawImage(preview, ltx, lty, width, height);
+    draw: function (frame_name, preview_name, coord, canvasId) {
+        if (typeof (coord) == 'string') {
+            coord = JSON.parse(coord)
         }
-      }
-    }
-  }
+
+        const screenWidth = window.innerWidth;
+        const canvas = document.getElementById(canvasId);
+
+        if (canvas.getContext) {
+            const ctx = canvas.getContext("2d");
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+            const frame = new Image();
+            frame.src = "/images/" + frame_name;
+
+            if (screenWidth < 640) {
+                canvas.width = 120
+                canvas.height = 80
+            }
+
+            const cw = canvas.width;
+            const ch = canvas.height;
+
+            frame.onload = function () {
+                const frameW = frame.width;
+                const frameH = frame.height;
+                const kfw = coord[0] / frameW;
+                const kfh = coord[1] / frameH;
+                const w = coord[6] - coord[0] + 1;
+                const h = coord[7] - coord[1] + 1;
+
+                const kw = cw / frameW;
+                const kh = ch / frameH;
+
+                const preview = new Image();
+                preview.src = preview_name;
+                preview.onload = function () {
+                    const width = (w * kw) < 10 && cw || (w * kw);
+                    const height = (h * kh) < 10 && ch || (h * kh);
+
+                    const lty = ch * kfh;
+                    const ltx = cw * kfw;
+
+                    ctx.drawImage(frame, 0, 0, cw, ch);
+                    ctx.drawImage(preview, ltx, lty, width, height);
+                }
+            }
+        }
+    },
 }
 
 export default Preview;
