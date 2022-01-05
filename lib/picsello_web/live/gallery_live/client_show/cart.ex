1bcd87cfb646d1863a62449c43bae62d0f0a1451
sviatosluv <sviatoslav.flud@litslink.com>
2022-01-05 11:40:42 +0200

    show ship options

--- lib/picsello_web/live/gallery_live/client_show/cart.ex
+++ lib/picsello_web/live/gallery_live/client_show/cart.ex
@@ -2,6 +2,7 @@ defmodule PicselloWeb.GalleryLive.ClientShow.Cart do
   @moduledoc false
   use PicselloWeb, live_view: [layout: "live_client"]
   alias Picsello.Cart
+  alias Picsello.WHCC.Shipping
 
   @impl true
   def mount(_params, _session, %{assigns: %{gallery: gallery}} = socket) do
@@ -9,6 +10,7 @@ defmodule PicselloWeb.GalleryLive.ClientShow.Cart do
       {:ok, order} ->
         socket
         |> assign(:order, order)
+        |> assign(:step, :product_list)
         |> ok()
 
       _ ->
@@ -19,4 +21,54 @@ defmodule PicselloWeb.GalleryLive.ClientShow.Cart do
         |> ok()
     end
   end
+
+  def handle_event("continue", _, %{assigns: %{step: :product_list}} = socket) do
+    socket
+    |> assign(:step, :shipping_opts)
+    |> assign_shipping_opts()
+    |> noreply()
+  end
+
+  def handle_event("click", params, %{assigns: %{step: :shipping_opts}} = socket) do
+    IO.inspect(socket)
+
+    socket
+    # |> assign(:step, :shipping_opts)
+    # |> assign_shipping_opts()
+    |> noreply()
+  end
+
+  defp assign_shipping_opts(%{assigns: %{order: %{products: products}}} = socket) do
+    # IO.inspect Enum.map(products, fn product -> shipping_opts_for_product(product) end)
+    socket
+    |> assign(
+      :shipping_opts,
+      Enum.map(products, fn product -> shipping_opts_for_product(product) end)
+    )
+  end
+
+  defp display_shipping_opts(assigns) do
+    ~H"""
+    <form>
+      <%= for option <- @options do %>
+        <%= render_slot(@inner_block, option) %>
+      <% end %>
+    </form>
+    """
+  end
+
+  defp shipping_opts_for_product(%{
+         editor_details: %{"product_id" => product_id, "selections" => %{"size" => size}}
+       }) do
+    %{product_id: product_id, list: Shipping.options(size)}
+    |> (&Map.put(&1, :current, List.first(&1[:list]))).()
+  end
+
+  defp shipping_opts_for_product(opts, %{editor_details: %{"product_id" => product_id}}) do
+    Enum.find(opts, fn %{product_id: id} -> id == product_id end)
+    |> (& &1[:list]).()
+  end
+
+  defp shipping_option_uid({uid, _, _, _}), do: uid
+  defp shipping_option_label({_, label, _, _}), do: label
 end
