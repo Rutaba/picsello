610fe45ed3200dec1f15f0a00289f42771d658d8
Serhii Lukianov <serhii.lukianov@litslink.com>
2022-01-10 16:04:33 +0200

    GAL-77: fixed order flow

--- lib/picsello_web/live/gallery_live/client_order.ex
+++ lib/picsello_web/live/gallery_live/client_order.ex
@@ -3,6 +3,7 @@ defmodule PicselloWeb.GalleryLive.ClientOrder do
 
   use PicselloWeb, live_view: [layout: "live_client"]
   alias Picsello.Cart
+  alias Picsello.Cart.Order
   alias Picsello.GalleryProducts
   alias Picsello.Galleries
 
@@ -17,21 +18,35 @@ defmodule PicselloWeb.GalleryLive.ClientOrder do
         _,
         %{assigns: %{gallery: gallery, live_action: :paid}} = socket
       ) do
+    order_id = to_integer(order_id)
+
     with {:ok, %{id: ^order_id} = order} <- Cart.get_unconfirmed_order(gallery.id) do
-      Cart.confirm_order(order, Galleries.account_id(gallery))
+      order = Cart.confirm_order(order, Galleries.account_id(gallery))
+
+      socket
+      |> assign(:from_checkout, true)
+      |> assign_details(gallery, order)
+      |> noreply()
+    else
+      _ ->
+        socket
+        |> push_patch(
+          to: Routes.gallery_client_order_path(socket, :show, gallery.client_link_hash, order_id)
+        )
+        |> noreply()
     end
+  end
+
+  def handle_params(%{"order_id" => order_id}, _, %{assigns: %{gallery: gallery}} = socket) do
+    %Order{} = order = Cart.get_placed_gallery_order(order_id, gallery.id)
 
     socket
-    |> assign(:from_checkout, true)
-    |> push_patch(
-      to: Routes.gallery_client_order_path(socket, :show, gallery.client_link_hash, order_id)
-    )
+    |> assign(:from_checkout, false)
+    |> assign_details(gallery, order)
     |> noreply()
   end
 
-  def handle_params(%{"order_id" => order_id}, _, %{assigns: %{gallery: gallery}} = conn) do
-    order = Cart.get_placed_gallery_order(order_id, gallery.id)
-
+  defp assign_details(conn, gallery, order) do
     gallery =
       gallery
       |> Galleries.populate_organization()
@@ -41,7 +56,6 @@ defmodule PicselloWeb.GalleryLive.ClientOrder do
     conn
     |> assign(:order, order)
     |> assign(:organization_name, org_name)
-    |> noreply()
   end
 
   defp product_description(%{id: id}) do
--- lib/picsello_web/live/gallery_live/client_show.ex
+++ lib/picsello_web/live/gallery_live/client_show.ex
@@ -1,7 +1,10 @@
 defmodule PicselloWeb.GalleryLive.ClientShow do
   @moduledoc false
 
-  use PicselloWeb, live_view: [layout: "live_client"]
+  use PicselloWeb,
+      live_view: [
+        layout: "live_client"
+      ]
   alias Picsello.Galleries
   alias Picsello.GalleryProducts
   alias Picsello.Cart
@@ -9,7 +12,15 @@ defmodule PicselloWeb.GalleryLive.ClientShow do
   @per_page 12
 
   @impl true
-  def handle_params(%{"editorId" => whcc_editor_id}, _, %{assigns: %{gallery: gallery}} = socket) do
+  def handle_params(
+        %{"editorId" => whcc_editor_id},
+        _,
+        %{
+          assigns: %{
+            gallery: gallery
+          }
+        } = socket
+      ) do
     socket
     |> place_product_in_cart(whcc_editor_id)
     |> push_redirect(to: Routes.gallery_client_show_path(socket, :show, gallery.client_link_hash))
@@ -17,7 +28,15 @@ defmodule PicselloWeb.GalleryLive.ClientShow do
   end
 
   @impl true
-  def handle_params(_params, _, %{assigns: %{gallery: gallery}} = socket) do
+  def handle_params(
+        _params,
+        _,
+        %{
+          assigns: %{
+            gallery: gallery
+          }
+        } = socket
+      ) do
     socket
     |> assign(:page_title, "Show Gallery")
     |> assign(:products, GalleryProducts.get_gallery_products(gallery.id))
@@ -31,7 +50,15 @@ defmodule PicselloWeb.GalleryLive.ClientShow do
   end
 
   @impl true
-  def handle_event("load-more", _, %{assigns: %{page: page}} = socket) do
+  def handle_event(
+        "load-more",
+        _,
+        %{
+          assigns: %{
+            page: page
+          }
+        } = socket
+      ) do
     socket
     |> assign(page: page + 1)
     |> assign(:update_mode, "append")
@@ -40,7 +67,15 @@ defmodule PicselloWeb.GalleryLive.ClientShow do
   end
 
   @impl true
-  def handle_event("toggle_favorites", _, %{assigns: %{favorites_filter: toggle_state}} = socket) do
+  def handle_event(
+        "toggle_favorites",
+        _,
+        %{
+          assigns: %{
+            favorites_filter: toggle_state
+          }
+        } = socket
+      ) do
     socket
     |> assign(:page, 0)
     |> assign(:update_mode, "replace")
@@ -51,53 +86,92 @@ defmodule PicselloWeb.GalleryLive.ClientShow do
 
   def handle_event(
         "product_preview_photo_click",
-        %{"template_id" => template_id, "photo_id" => photo_id},
-        %{assigns: %{products: products}} = socket
+        %{"params" => id},
+        %{
+          assigns: %{
+            products: products
+          }
+        } = socket
       ) do
-    gallery_product = Enum.find(products, fn product -> product.id == String.to_integer(template_id) end)
-    photo = Galleries.get_photo(String.to_integer(photo_id))
+    gallery_product = Enum.find(products, fn product -> product.id == String.to_integer(id) end)
 
     socket
-    |> open_modal(PicselloWeb.GalleryLive.EditProduct, %{
-      category_template: gallery_product.category_template,
-      photo: photo
-    })
+    |> open_modal(
+         PicselloWeb.GalleryLive.EditProduct,
+         %{
+           category_template: gallery_product.category_template,
+           photo: gallery_product.preview_photo
+         }
+       )
     |> noreply()
   end
-
   @impl true
   def handle_event(
         "click",
         %{"preview_photo_id" => photo_id},
-        %{assigns: %{gallery: gallery, favorites_filter: favorites_filter}} = socket
+        %{
+          assigns: %{
+            gallery: gallery,
+            favorites_filter: favorites_filter
+          }
+        } = socket
       ) do
     photo_ids =
       Galleries.get_photo_ids(gallery_id: gallery.id, favorites_filter: favorites_filter)
 
     socket
-    |> open_modal(PicselloWeb.GalleryLive.ChooseProduct, %{
-      gallery: gallery,
-      photo_id: photo_id,
-      photo_ids:
-        CLL.init(photo_ids)
-        |> CLL.next(photo_ids |> Enum.find_index(&(&1 == to_integer(photo_id))))
-    })
+    |> open_modal(
+         PicselloWeb.GalleryLive.ChooseProduct,
+         %{
+           gallery: gallery,
+           photo_id: photo_id,
+           photo_ids:
+             CLL.init(photo_ids)
+             |> CLL.next(
+                  photo_ids
+                  |> Enum.find_index(&(&1 == to_integer(photo_id)))
+                )
+         }
+       )
     |> noreply
   end
 
   @impl true
-  def handle_info(:increase_favorites_count, %{assigns: %{favorites_count: count}} = socket) do
-    socket |> assign(:count, count + 1) |> noreply()
+  def handle_info(
+        :increase_favorites_count,
+        %{
+          assigns: %{
+            favorites_count: count
+          }
+        } = socket
+      ) do
+    socket
+    |> assign(:count, count + 1)
+    |> noreply()
   end
 
   @impl true
-  def handle_info(:reduce_favorites_count, %{assigns: %{favorites_count: count}} = socket) do
-    socket |> assign(:count, count - 1) |> noreply()
+  def handle_info(
+        :reduce_favorites_count,
+        %{
+          assigns: %{
+            favorites_count: count
+          }
+        } = socket
+      ) do
+    socket
+    |> assign(:count, count - 1)
+    |> noreply()
   end
 
   def handle_info(
         {:photo_click, photo},
-        %{assigns: %{gallery: gallery, favorites_filter: favorites?}} = socket
+        %{
+          assigns: %{
+            gallery: gallery,
+            favorites_filter: favorites?
+          }
+        } = socket
       ) do
     created_editor =
       Picsello.WHCC.create_editor(
@@ -105,7 +179,7 @@ defmodule PicselloWeb.GalleryLive.ClientShow do
         photo,
         complete_url:
           Routes.gallery_dump_editor_url(socket, :show, gallery.client_link_hash) <>
-            "?editorId=%EDITOR_ID%",
+          "?editorId=%EDITOR_ID%",
         cancel_url: Routes.gallery_client_show_url(socket, :show, gallery.client_link_hash),
         only_favorites: favorites?
       )
@@ -117,7 +191,11 @@ defmodule PicselloWeb.GalleryLive.ClientShow do
 
   def handle_info(
         {:customize_and_buy_product, whcc_product, photo},
-        %{assigns: %{gallery: gallery}} = socket
+        %{
+          assigns: %{
+            gallery: gallery
+          }
+        } = socket
       ) do
     created_editor =
       Picsello.WHCC.create_editor(
@@ -125,7 +203,7 @@ defmodule PicselloWeb.GalleryLive.ClientShow do
         photo,
         complete_url:
           Routes.gallery_client_show_url(socket, :show, gallery.client_link_hash) <>
-            "?editorId=%EDITOR_ID%",
+          "?editorId=%EDITOR_ID%",
         cancel_url: Routes.gallery_client_show_url(socket, :show, gallery.client_link_hash)
       )
 
@@ -134,7 +212,14 @@ defmodule PicselloWeb.GalleryLive.ClientShow do
     |> noreply()
   end
 
-  defp place_product_in_cart(%{assigns: %{gallery: gallery}} = socket, whcc_editor_id) do
+  defp place_product_in_cart(
+         %{
+           assigns: %{
+             gallery: gallery
+           }
+         } = socket,
+         whcc_editor_id
+       ) do
     gallery_account_id = Galleries.account_id(gallery)
     cart_product = Cart.new_product(whcc_editor_id, gallery_account_id)
     Cart.place_product(cart_product, gallery.id)
@@ -155,17 +240,19 @@ defmodule PicselloWeb.GalleryLive.ClientShow do
   end
 
   def get_menu_items(_socket),
-    do: [
-      %{title: "Home", path: "#"},
-      %{title: "Shop", path: "#"},
-      %{title: "My orders", path: "#"},
-      %{title: "Help", path: "#"}
-    ]
+      do: [
+        %{title: "Home", path: "#"},
+        %{title: "Shop", path: "#"},
+        %{title: "My orders", path: "#"},
+        %{title: "Help", path: "#"}
+      ]
 
   defp assign_photos(
          %{
            assigns: %{
-             gallery: %{id: id},
+             gallery: %{
+               id: id
+             },
              page: page,
              favorites_filter: filter
            }
@@ -176,7 +263,15 @@ defmodule PicselloWeb.GalleryLive.ClientShow do
     photos = Galleries.get_gallery_photos(id, per_page + 1, page, opts)
 
     socket
-    |> assign(:photos, photos |> Enum.take(per_page))
-    |> assign(:has_more_photos, photos |> length > per_page)
+    |> assign(
+         :photos,
+         photos
+         |> Enum.take(per_page)
+       )
+    |> assign(
+         :has_more_photos,
+         photos
+         |> length > per_page
+       )
   end
 end
