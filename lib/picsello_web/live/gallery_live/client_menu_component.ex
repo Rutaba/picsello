a4e4f84e36fe22abe03700a49e1345a4a71500c7
Iryna Obertynska <iryna.obertynska@litslink.com>
2022-01-11 13:08:18 +0200

    formatting

--- lib/picsello_web/live/gallery_live/client_menu_component.ex
+++ lib/picsello_web/live/gallery_live/client_menu_component.ex
@@ -1,12 +1,12 @@
 defmodule PicselloWeb.GalleryLive.ClientMenuComponent do
-	@moduledoc false
-	use PicselloWeb, :live_component
+  @moduledoc false
+  use PicselloWeb, :live_component
 
-	def get_menu_items(_socket),
-			do: [
-				%{title: "Home", path: "#"},
-				%{title: "Shop", path: "#"},
-				%{title: "My orders", path: "#"},
-				%{title: "Help", path: "#"}
-			]
+  def get_menu_items(_socket),
+    do: [
+      %{title: "Home", path: "#"},
+      %{title: "Shop", path: "#"},
+      %{title: "My orders", path: "#"},
+      %{title: "Help", path: "#"}
+    ]
 end
