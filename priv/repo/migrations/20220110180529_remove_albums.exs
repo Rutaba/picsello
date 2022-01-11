f17a2e78499eb0248d2898f835a20f4d003fd2ec
Ihor Chernov <ihorchernov@Macs-MacBook-Pro.local>
2022-01-11 18:53:14 +0200

    added share_gallery button to setting page

--- priv/repo/migrations/20220110180529_remove_albums.exs
+++ priv/repo/migrations/20220110180529_remove_albums.exs
@@ -2,10 +2,10 @@ defmodule Picsello.Repo.Migrations.RemoveAlbums do
   use Ecto.Migration
 
   def change do
+
     alter table("photos") do
       remove :album_id
     end
-
     drop table("albums")
   end
 end
