f2bb0de986730f80b863b6ee7434adc7833e1c52
Brian Dunn <brian@theophil.us>
2022-01-13 17:29:48 -0600

    Update profile design
    https://picsello.atlassian.net/browse/PIC-428

--- test/picsello_web/live/user/settings_test.exs
+++ test/picsello_web/live/user/settings_test.exs
@@ -37,9 +37,13 @@ defmodule PicselloWeb.Live.User.SettingsTest do
       |> Picsello.Repo.update!()
 
       {:ok, _view, html} = live(conn, Routes.user_settings_path(conn, :edit))
-      assert [form] = html |> Floki.parse_fragment!() |> Floki.find("main > :not(header) form")
       sign_out_path = Routes.user_session_path(conn, :delete)
-      assert [^sign_out_path] = form |> Floki.attribute("action")
+
+      assert [^sign_out_path] =
+               html
+               |> Floki.parse_fragment!()
+               |> Floki.attribute("main > :not(header) form", "action")
+               |> Enum.uniq()
     end
   end
 
