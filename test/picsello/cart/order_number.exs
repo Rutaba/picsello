1861b33050d02b20fe5562195cc7163145b3e6a3
Serhii Lukianov <serhii.lukianov@litslink.com>
2022-01-20 14:29:34 +0200

    number convertion

--- test/picsello/cart/order_number.exs
+++ test/picsello/cart/order_number.exs
@@ -7,13 +7,41 @@ defmodule Picsello.Cart.OrderNumberTest do
     test "number greater than half a base" do
       half = trunc(@base / 2)
 
-      for i <- 1..100_000_000 do
+      for i <- [1, 100, 1000, 30000, 10_000_000] do
         assert to_number(i) > half
       end
     end
   end
 
-  defp to_number(int) do
+  @divisor 4_294_967_296
+
+  def to_number(int) when is_integer(int) and int > 0 do
     int
+    |> rem(@divisor)
+    |> :binary.encode_unsigned()
+    |> mangle_bits()
+    |> then(fn mangled ->
+      int
+      |> Integer.floor_div(@divisor)
+      |> then(fn head -> head * @divisor + :binary.decode_unsigned(mangled) end)
+    end)
+  end
+
+  defp mangle_bits(<<x::24>>), do: mangle_bits(<<0, x>>)
+  defp mangle_bits(<<x::16>>), do: mangle_bits(<<0, 0, x>>)
+  defp mangle_bits(<<x::8>>), do: mangle_bits(<<0, 0, 0, x>>)
+
+  defp mangle_bits(<<a::3, b::7, c::5, d::11, e::2, f::1, g::1, h::1, i::1>>) do
+    <<
+      Bitwise.bxor(b, 0b0110110)::7,
+      i::1,
+      Bitwise.bxor(d, 0b10010010011)::11,
+      h::1,
+      Bitwise.bxor(a, 0b110)::3,
+      g::1,
+      Bitwise.bxor(e, 0b01)::2,
+      f::1,
+      Bitwise.bxor(c, 0b10101)::5
+    >>
   end
 end
