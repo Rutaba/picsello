4e62efc199a883a691ed305c28af9577934b9eaf
Serhii Lukianov <serhii.lukianov@litslink.com>
2022-01-25 17:10:24 +0200

    fix test

--- test/features/client_orders_test.exs
+++ test/features/client_orders_test.exs
@@ -9,12 +9,16 @@ defmodule Picsello.ClientOrdersTest do
     cart_products =
       Enum.map(1..3, fn _ -> build(:ordered_cart_product, %{product_id: whcc_product.whcc_id}) end)
 
-    [order: insert(:confirmed_order, %{gallery_id: gallery.id, products: cart_products})]
+    [
+      order:
+        insert(:confirmed_order, %{gallery_id: gallery.id, products: cart_products})
+        |> Picsello.Cart.set_order_number()
+    ]
   end
 
   feature "client reviews the placed order", %{session: session, gallery: gallery, order: order} do
     session
-    |> visit("/gallery/#{gallery.client_link_hash}/orders/#{order.id}")
+    |> visit("/gallery/#{gallery.client_link_hash}/orders/#{order.number}")
     |> assert_text("My orders")
     |> assert_text("Order number #{order.number}")
     |> assert_text("Your order will be sent to:")
