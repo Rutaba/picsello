800d8fe76c453991c82511a1f711c0313c4a5dc9
lucas <lucas.ocampos0418@gmail.com>
2022-03-14 05:43:29 +1100

    Cosmetic changes

--- lib/picsello_web/live/gallery_live/albums.ex
+++ lib/picsello_web/live/gallery_live/albums.ex
@@ -3,29 +3,30 @@ defmodule PicselloWeb.GalleryLive.Albums do
   use PicselloWeb, live_view: [layout: "live_client"]
 
   alias Picsello.Galleries
-  alias Picsello.Galleries.Album
   alias Picsello.Repo
 
   @impl true
   def mount(%{"gallery_id" => gallery_id}, _session, socket) do
-    albums = Repo.all(Album, gallery_id: gallery_id)
-    gallery = Galleries.get_gallery!(gallery_id)
+    gallery = Galleries.get_gallery!(gallery_id) |> Repo.preload(:albums)
 
     {
       :ok,
       socket
       |> assign(:gallery_id, gallery_id)
       |> assign(:gallery, gallery)
-      |> assign(:albums, albums)
     }
   end
 
   @impl true
-  def handle_event("open_albums_popup", %{},  %{
-    assigns: %{
-      gallery_id: gallery_id
-    }
-  } = socket) do
+  def handle_event(
+        "open_albums_popup",
+        %{},
+        %{
+          assigns: %{
+            gallery_id: gallery_id
+          }
+        } = socket
+      ) do
     socket
     |> open_modal(PicselloWeb.GalleryLive.Settings.AddAlbumModal, %{gallery_id: gallery_id})
     |> noreply()
