9d9d250c25c201a2ee90de5910a0c8c499bb9d44
lucas <lucas.ocampos0418@gmail.com>
2022-03-15 23:29:15 +1100

    Updated Footer design and added footer component

---
+++ lib/picsello_web/live/gallery_live/shared/footer_component.ex
@@ -0,0 +1,9 @@
+defmodule PicselloWeb.GalleryLive.Shared.FooterComponent do
+  @moduledoc false
+  use PicselloWeb, :live_component
+
+  @impl true
+  def update(_params, socket) do
+    {:ok, socket}
+  end
+end
