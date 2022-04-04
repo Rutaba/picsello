5498a620a1ade4b3df00eec23a6b3e8bb5bcf0cd
Brian Dunn <brian@theophil.us>
2022-04-04 18:10:24 -0500

    responsive full screen

--- lib/picsello_web/live/gallery_live/client_orders.ex
+++ lib/picsello_web/live/gallery_live/client_orders.ex
@@ -8,8 +8,7 @@ defmodule PicselloWeb.GalleryLive.ClientOrders do
   def handle_params(_, _, %{assigns: %{gallery: gallery}} = socket) do
     orders = Cart.get_orders(gallery.id)
 
-    %{job: %{client: %{organization: %{user: %{time_zone: time_zone}}}}} =
-      gallery = Galleries.populate_organization_user(gallery)
+    gallery = Galleries.populate_organization_user(gallery)
 
     socket
     |> assign(gallery: gallery, orders: orders)
@@ -17,12 +16,42 @@ defmodule PicselloWeb.GalleryLive.ClientOrders do
     |> noreply()
   end
 
-  defp time_zone(%{job: %{client: %{organization: %{user: %{time_zone: time_zone}}}}}),
-    do: time_zone
+  defp order_date(
+         %{job: %{client: %{organization: %{user: %{time_zone: time_zone}}}}},
+         %{placed_at: placed_at},
+         format
+       ),
+       do: strftime(time_zone, placed_at, format)
+
+  defp item_frame(assigns) do
+    assigns = Enum.into(assigns, %{quantity: [], shipping: []})
+
+    ~H"""
+      <div class="block py-6 lg:justify-between lg:py-8 lg:flex">
+        <div class="grid gap-4 grid-cols-[120px,1fr,min-content] lg:grid-cols-[147px,1fr]">
+          <img src={preview_url(@item)} class="object-contain h-32 lg:h-[120px] place-self-center"/>
+
+          <div class="flex flex-col justify-center py-2 align-self-center">
+            <div class="flex items-baseline lg:flex-col">
+              <span class="mr-2 text-lg lg:text-base lg:font-medium"><%= product_name(@item) %></span>
+              <span class="text-lg font-extrabold lg:mt-2"><%= @item.price %></span>
+            </div>
+
+            <%= render_slot(@quantity) %>
+          </div>
+
+        </div>
+
+        <%= render_slot(@shipping) %>
+      </div>
+    """
+  end
 
   defp quantity(%{editor_details: %{selections: %{"quantity" => quantity}}}), do: quantity
+  defp quantity(_), do: 0
 
   defdelegate total_cost(order), to: Cart
   defdelegate preview_url(item), to: Cart
-  defdelegate product_name(item), to: Cart
+  defp product_name(%Picsello.Cart.Digital{}), do: "Digital download"
+  defp product_name(item), do: Cart.product_name(item)
 end
