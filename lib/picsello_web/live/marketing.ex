1eb4873279d32d3c2901aecc3001db51f20325c8
Kyle Bacon <hello@kylebaconcreative.com>
2022-05-18 09:47:25 -0700

    Add next up section

--- lib/picsello_web/live/marketing.ex
+++ lib/picsello_web/live/marketing.ex
@@ -7,16 +7,99 @@ defmodule PicselloWeb.Live.Marketing do
   def mount(_params, _session, socket) do
     socket
     |> assign(:page_title, "Marketing")
+    |> assign_attention_items()
     |> assign_organization()
     |> assign_campaigns()
     |> ok()
   end
 
   @impl true
+  def handle_event("public-profile", %{}, socket),
+    do:
+      socket
+      |> push_redirect(to: Routes.profile_settings_path(socket, :index))
+      |> noreply()
+
+  def assign_attention_items(socket) do
+    items = [
+      %{
+        action: "public-profile",
+        title: "Review your Public Profile",
+        body:
+          "We highly suggest you review your Picsello Public Profile. We provide options to insert links into your emails (wardrobe guide, pricing, etc)",
+        icon: "three-people",
+        button_label: "Take me to settings",
+        button_class: "btn-secondary",
+        external_link: "",
+        color: "purple-marketing-300",
+        class: "border-purple-marketing-300"
+      },
+      %{
+        action: "external-link",
+        title: "Marketing tip: SEO",
+        body:
+          "Google loves their own products. Rank higher in search by adding a YouTube Video or Google Maps to your website!",
+        icon: "three-people",
+        button_label: "Check out our blog",
+        button_class: "btn-secondary",
+        external_link:
+          "https://www.picsello.com/post/best-business-resources-for-new-photographers",
+        color: "purple-marketing-300",
+        class: "border-purple-marketing-300"
+      },
+      %{
+        action: "external-link",
+        title: "Marketing tip: SEO",
+        body:
+          "Setup Google My Business if you haven’t already and ask for more reviews—they really help your search results!",
+        icon: "three-people",
+        button_label: "Check out our blog",
+        button_class: "btn-secondary",
+        external_link:
+          "https://www.picsello.com/post/best-business-resources-for-new-photographers",
+        color: "purple-marketing-300",
+        class: "border-purple-marketing-300"
+      }
+    ]
+
+    socket |> assign(:attention_items, items)
+  end
+
+  @impl true
   def render(assigns) do
     ~H"""
-    <header>
-      <h1 class="px-6 py-10 text-4xl font-semibold center-container">Marketing</h1>
+    <header class="bg-gray-100">
+      <div class="pt-10 pb-8 center-container">
+        <h1 class="px-6 text-4xl font-bold">Marketing</h1>
+        <%= case @attention_items do %>
+        <% [] -> %>
+        <% items -> %>
+          <h2 class="px-6 mt-8 mb-4 text-sm font-bold tracking-widest text-gray-400 uppercase">Next Up</h2>
+          <ul class="flex px-6 pb-4 overflow-auto lg:pb-0 lg:overflow-none intro-next-up">
+            <%= for %{title: title, body: body, icon: icon, button_label: button_label, button_class: button_class, color: color, action: action, class: class, external_link: external_link} <- items do %>
+            <li {testid("marketing-attention-item")} class={"flex-shrink-0 flex lg:flex-1 flex-col justify-between max-w-sm w-3/4 p-5 cursor-pointer mr-4 border rounded-lg #{class} bg-white border-gray-250"}>
+              <div>
+                <h1 class="text-lg font-bold">
+                  <.icon name={icon} width="23" height="20" class={"inline-block mr-2 rounded-sm fill-current bg-blue-planning-100 text-#{color}"} />
+                  <%= title %>
+                </h1>
+
+                <p class="my-2 text-sm"><%= body %></p>
+
+                <%= case action do %>
+                  <% "public-profile" -> %>
+                    <button type="button" phx-click={action} class={"#{button_class} text-sm w-full py-2 mt-2"}><%= button_label %></button>
+                  <% _ -> %>
+                    <a href={external_link} class={"#{button_class} text-center text-sm w-full py-2 mt-2 inline-block"} target="_blank" rel="noopener noreferrer">
+                      <%= button_label %>
+                    </a>
+                <% end %>
+              </div>
+            </li>
+            <% end %>
+          </ul>
+      <% end %>
+      </div>
     </header>
     <div class="px-6 center-container" {intro(@current_user, "intro_marketing")}>
       <div class="mx-0 mt-8 pb-32 sm:pb-0 grid grid-cols-1 lg:grid-cols-2 gap-x-9 gap-y-6">
