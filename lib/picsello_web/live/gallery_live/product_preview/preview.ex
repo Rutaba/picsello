bfbfaa263e77edf300ecdff166b0f61439375e4f
Morgan Ziegler <morganziegler@morgans-mbp-2.myfiosgateway.com>
2022-08-16 13:25:19 -0400

    Text changes and more set up

--- lib/picsello_web/live/gallery_live/product_preview/preview.ex
+++ lib/picsello_web/live/gallery_live/product_preview/preview.ex
@@ -11,26 +11,25 @@ defmodule PicselloWeb.GalleryLive.ProductPreview.Preview do
       <div class="items-center mt-8">
         <div class={classes("flex items-center pt-4 font-sans lg:text-lg text-2xl font-bold", %{"text-gray-400" => @category.coming_soon})}>
           <%= @category.name %>
-
         </div>
+
         <div class=" mx-4 pt-4 flex flex-col justify-between" >
           <label class="toggle">
-            <input class="toggle-checkbox" type="checkbox" phx-click="" checked>
+            <input id = "enable_sell_toggle" class="toggle-checkbox" type="checkbox" phx-click="" checked>
             <div class="toggle-switch"></div>
-            <span class="toggle-label">Product Enabled</span>
+            <span class="toggle-label">Product enabled to sell</span>
           </label>
         </div>
 
-
         <div class={classes("mt-4 pb-4 bg-gray-200", %{"bg-gray-200/20" => @category.coming_soon})}>
         <div class=" mx-4 pt-4 flex flex-col justify-between">
+
         <label class="toggle">
-          <input class="toggle-checkbox" type="checkbox" phx-click="" checked>
+          <input id="preview_toggle" class="toggle-checkbox" type="checkbox" phx-click="" checked>
           <div class="toggle-switch"></div>
           <span class="toggle-label">Show product preview in gallery</span>
         </label>
       </div>
-
 
           <div class="flex items-center justify-center mt-4">
             <.framed_preview category={@category} photo={@photo} width={cards_width(@category.frame_image)}/>
@@ -46,8 +45,8 @@ defmodule PicselloWeb.GalleryLive.ProductPreview.Preview do
 
               <button
               class="flex items-center font-sans text-sm py-2 pr-3.5 pl-3 bg-white border border-blue-planning-300 rounded-lg cursor-pointer"
-              id={"product-id-#{@product_id}"}
               phx-click="edit"
+              id={"product-id-#{@product_id}"}
               phx-value-product_id={@product_id}>
                 <.icon name="pencil" class="mr-2.5 w-3 h-3 fill-current text-blue-planning-300" />
                 <span>Edit product preview</span>
