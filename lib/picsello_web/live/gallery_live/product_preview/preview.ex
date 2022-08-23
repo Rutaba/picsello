b4b75f79881c8533008ba85f0108f027494b37ae
Brian Dunn <brian@theophil.us>
2022-08-22 20:07:30 -0500

    Implement card frame and auto-orientation

--- lib/picsello_web/live/gallery_live/product_preview/preview.ex
+++ lib/picsello_web/live/gallery_live/product_preview/preview.ex
@@ -26,47 +26,59 @@ defmodule PicselloWeb.GalleryLive.ProductPreview.Preview do
   @spec render(any) :: Phoenix.LiveView.Rendered.t()
   def render(assigns) do
     ~H"""
-    <div class="flex flex-col justify-between">
-      <div class="items-center mt-8">
-        <div class={classes("flex items-center pt-4 font-sans lg:text-lg text-2xl font-bold", %{"text-gray-400" => @category.coming_soon})}>
-          <%= @category.name %>
-        </div>
-        <div class=" mx-4 pt-4 flex flex-col justify-between" >
-          <.toggle_preview click="sell_product_enabled" checked={@product.sell_product_enabled} text="Product enabled to sell" myself={@myself} />
-        </div>
-
-        <div class={classes("mt-4 pb-4 bg-gray-200", %{"bg-gray-200/20" => @category.coming_soon})}>
+      <div class="flex flex-col justify-between">
+        <div class="items-center mt-8">
+          <div class={classes("flex items-center pt-4 font-sans lg:text-lg text-2xl font-bold", %{"text-gray-400" => @category.coming_soon})}>
+            <%= @category.name %>
+          </div>
           <div class=" mx-4 pt-4 flex flex-col justify-between" >
-            <%= if @product.sell_product_enabled do %>
-            <.toggle_preview click="product_preview_enabled" checked={@product.product_preview_enabled} text="Show product preview in gallery" myself={@myself} />
-            <% end %>
+            <.toggle_preview click="sell_product_enabled" checked={@product.sell_product_enabled} text="Product enabled to sell" myself={@myself} />
           </div>
 
-          <div class="flex items-center justify-center mt-4">
-            <.framed_preview category={@category} photo={@photo}/>
-          </div>
-
-          <div class="flex justify-start pt-4 pl-4">
-            <%= if @category.coming_soon do %>
-               <button class="p-2 font-bold rounded-lg text-blue-planning-300 bg-blue-planning-100" disabled>
-                Coming soon!
-              </button>
-            <% else %>
-              <%= if @product.product_preview_enabled and @product.sell_product_enabled do %>
-                <button
-                class="flex items-center font-sans text-sm py-2 pr-3.5 pl-3 bg-white border border-blue-planning-300 rounded-lg cursor-pointer"
-                phx-click="edit"
-                id={"product-id-#{@product_id}"}
-                phx-value-product_id={@product_id}>
-                  <.icon name="pencil" class="mr-2.5 w-3 h-3 fill-current text-blue-planning-300" />
-                  <span>Edit product preview</span>
-                </button>
+          <div class={classes("mt-4 pb-4 bg-gray-200", %{"bg-gray-200/20" => @category.coming_soon})}>
+            <div class=" mx-4 pt-4 flex flex-col justify-between" >
+              <%= if @product.sell_product_enabled do %>
+              <.toggle_preview click="product_preview_enabled" checked={@product.product_preview_enabled} text="Show product preview in gallery" myself={@myself} />
               <% end %>
-            <% end %>
+            </div>
+
+            <div class={classes("mt-4 pb-4 bg-gray-200", %{"bg-gray-200/20" => @category.coming_soon})}>
+              <div class=" mx-4 pt-4 flex flex-col justify-between">
+              <%= if @product.enabled do %>
+                <label class="toggle">
+                  <input class="toggle-checkbox" type="checkbox" phx-click="preview_enabled" checked={@product.preview_enabled} phx-target={@myself}>
+                  <div class="toggle-switch"></div>
+                  <span class="toggle-label">Show product preview in gallery</span>
+                </label>
+              <% end %>
+              </div>
+
+              <div class="flex justify-start pt-4 pl-4">
+                <%= if @category.coming_soon do %>
+                  <button class="p-2 font-bold rounded-lg text-blue-planning-300 bg-blue-planning-100" disabled>
+                    Coming soon!
+                  </button>
+                <% else %>
+                  <%= if @product.product_preview_enabled and @product.sell_product_enabled do %>
+                    <button
+                    class="flex items-center font-sans text-sm py-2 pr-3.5 pl-3 bg-white border border-blue-planning-300 rounded-lg cursor-pointer"
+                    phx-click="edit"
+                    id={"product-id-#{@product_id}"}
+                    phx-value-product_id={@product_id}>
+                      <.icon name="pencil" class="mr-2.5 w-3 h-3 fill-current text-blue-planning-300" />
+                      <span>Edit product preview</span>
+                    </button>
+                  <% end %>
+                <% end %>
+              </div>
+
+              <div class="flex items-center justify-center mt-4">
+                <.framed_preview category={@category} photo={@photo} />
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     """
   end
 
