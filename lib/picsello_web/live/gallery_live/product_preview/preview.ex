1d9329a6a770b4583e679680c494f52b2ca466eb
Ukasha Habib <ukasha@dycoders.com>
2022-10-03 14:31:31 -0400

    rebase main

--- lib/picsello_web/live/gallery_live/product_preview/preview.ex
+++ lib/picsello_web/live/gallery_live/product_preview/preview.ex
@@ -38,21 +38,11 @@ defmodule PicselloWeb.GalleryLive.ProductPreview.Preview do
           <div class={classes("mt-4 pb-4 bg-gray-200", %{"bg-gray-200/20" => @category.coming_soon})}>
             <div class=" mx-4 pt-4 flex flex-col justify-between" >
               <%= if @product.sell_product_enabled do %>
-              <.toggle_preview click="product_preview_enabled" checked={@product.product_preview_enabled} text="Show product preview in gallery" myself={@myself} />
+                <.toggle_preview click="product_preview_enabled" checked={@product.product_preview_enabled} text="Show product preview in gallery" myself={@myself} />
               <% end %>
             </div>
 
             <div class={classes("mt-4 pb-4 bg-gray-200", %{"bg-gray-200/20" => @category.coming_soon})}>
-              <div class=" mx-4 pt-4 flex flex-col justify-between">
-              <%= if @product.enabled do %>
-                <label class="toggle">
-                  <input class="toggle-checkbox" type="checkbox" phx-click="preview_enabled" checked={@product.preview_enabled} phx-target={@myself}>
-                  <div class="toggle-switch"></div>
-                  <span class="toggle-label">Show product preview in gallery</span>
-                </label>
-              <% end %>
-              </div>
-
               <div class="flex justify-start pt-4 pl-4">
                 <%= if @category.coming_soon do %>
                   <button class="p-2 font-bold rounded-lg text-blue-planning-300 bg-blue-planning-100" disabled>
@@ -85,9 +75,9 @@ defmodule PicselloWeb.GalleryLive.ProductPreview.Preview do
   defp toggle_preview(assigns) do
     ~H"""
     <label class="inline-flex relative items-center cursor-pointer">
-    <input type="checkbox" class="sr-only peer" phx-click={@click} checked={@checked} phx-target={@myself}>
-    <div class="w-11 h-6 bg-gray-300 rounded-full peer  peer-focus:ring-toggle-100 dark:peer-focus:ring-toggle-300 dark:bg-gray-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-toggle-100"></div>
-    <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300"><%= @text %></span>
+      <input type="checkbox" class="sr-only peer" phx-click={@click} checked={@checked} phx-target={@myself}>
+      <div class="w-11 h-6 bg-gray-300 rounded-full peer  peer-focus:ring-toggle-100 dark:peer-focus:ring-toggle-300 dark:bg-gray-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-toggle-100"></div>
+      <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300"><%= @text %></span>
     </label>
     """
   end
