80453084e7d1507b7b2656230dd5a0a0a55614b7
hrd-2 <hrd2@picsello.com>
2023-02-06 18:52:44 +0500

    set show_on_pub;ic_profile to false when user archives a package

--- lib/picsello/packages.ex
+++ lib/picsello/packages.ex
@@ -511,7 +511,7 @@ defmodule Picsello.Packages do
     from(p in Package,
       where: p.organization_id == ^organization_id and p.job_type == ^job_type
     )
-    |> Repo.update_all(set: [archived_at: DateTime.truncate(DateTime.utc_now(), :second)])
+    |> Repo.update_all(set: [archived_at: DateTime.truncate(DateTime.utc_now(), :second), show_on_public_profile: false])
   end
 
   def unarchive_packages_for_job_type(job_type, organization_id) do
