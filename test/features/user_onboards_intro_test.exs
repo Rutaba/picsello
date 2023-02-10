4a2d855734d1ee6374e8e5fd6c40cd6bbf90bdfd
Kyle Bacon <hello@kylebaconcreative.com>
2023-02-09 21:57:01 -0800

    How about now…stubborn

--- test/features/user_onboards_intro_test.exs
+++ test/features/user_onboards_intro_test.exs
@@ -7,81 +7,69 @@ defmodule Picsello.UserOnboardsIntroTest do
     [user: user]
   end
 
-  describe "view first time" do
-    setup :onboarded_show_intro
-    setup :authenticated
+  setup :onboarded_show_intro
+  setup :authenticated
 
-    feature "user gets welcome modal, clicks client booking", %{session: session} do
-      session
-      |> assert_text("Welcome to the Picsello Family!")
-      |> click(css(".welcome-column", count: 3, at: 0))
-      |> assert_path("/booking-events")
-      |> assert_text("Booking events")
-      |> visit("/home")
-      |> assert_text("Start product tour")
-    end
-
-    feature "user gets welcome modal, clicks galleries", %{session: session} do
-      session
-      |> assert_text("Welcome to the Picsello Family!")
-      |> click(css(".welcome-column", count: 3, at: 1))
-      |> assert_path("/galleries")
-      |> assert_text("Your Galleries")
-      |> visit("/home")
-      |> assert_text("Start product tour")
-    end
-
-    feature "user gets welcome modal, clicks demo", %{session: session} do
-      session
-      |> assert_text("Welcome to the Picsello Family!")
-      |> click(link("Join a demo"))
-      |> visit("/home")
-      |> refute_has(css("#welcome-text", text: "Welcome to the Picsello Family!"))
-    end
-
-    feature "user has intro js loaded", %{session: session} do
-      session
-      |> visit("/home")
-      |> find(Query.data("intro-show", "true"))
-    end
-
-    feature "users starts product tour and uses it", %{session: session} do
-      session
-      |> visit("/home")
-      |> sleep(1000)
-      |> within_modal(fn modal ->
-        modal
-        |> click(button("cancel"))
-      end)
-      |> click(css("#start-tour"))
-      |> click(css(".introjs-nextbutton"))
-      |> click(css(".introjs-nextbutton"))
-      |> click(css(".introjs-nextbutton"))
-      |> click(css(".introjs-nextbutton"))
-      |> click(css(".introjs-nextbutton"))
-      |> click(css(".introjs-nextbutton"))
-      |> click(css(".introjs-donebutton"))
-      |> visit("/home")
-      |> refute_has(css("#start-tour"))
-    end
+  feature "user gets welcome modal, clicks client booking", %{session: session} do
+    session
+    |> assert_text("Welcome to the Picsello Family!")
+    |> click(css(".welcome-column", count: 3, at: 0))
+    |> assert_path("/booking-events")
+    |> assert_text("Booking events")
+    |> visit("/home")
+    |> assert_text("Start product tour")
   end
 
-  describe "sees second time" do
-    setup :onboarded_show_intro
-    setup :authenticated
+  feature "user gets welcome modal, clicks galleries", %{session: session} do
+    session
+    |> assert_text("Welcome to the Picsello Family!")
+    |> click(css(".welcome-column", count: 3, at: 1))
+    |> assert_path("/galleries")
+    |> assert_text("Your Galleries")
+    |> visit("/home")
+    |> assert_text("Start product tour")
+  end
 
-    feature "user interacts with intro js tour and dismisses it", %{session: session} do
-      session
-      |> visit("/home")
-      |> sleep(1000)
-      |> within_modal(fn modal ->
-        modal
-        |> click(button("cancel"))
-      end)
-      |> click(css("#start-tour"))
-      |> click(css(".introjs-skipbutton"))
-      |> visit("/")
-      |> find(Query.data("intro-show", "false"))
-    end
+  feature "user gets welcome modal, clicks demo", %{session: session} do
+    session
+    |> assert_text("Welcome to the Picsello Family!")
+    |> click(link("Join a demo"))
+    |> visit("/home")
+    |> refute_has(css("#welcome-text", text: "Welcome to the Picsello Family!"))
+  end
+
+  feature "user has intro js loaded", %{session: session} do
+    session
+    |> visit("/home")
+    |> find(Query.data("intro-show", "true"))
+  end
+
+  feature "users starts product tour and uses it", %{session: session} do
+    session
+    |> visit("/home")
+    |> within_modal(fn modal ->
+      modal
+      |> click(button("cancel"))
+    end)
+    |> click(css("#start-tour"))
+    |> click(css(".introjs-nextbutton"))
+    |> click(css(".introjs-nextbutton"))
+    |> click(css(".introjs-nextbutton"))
+    |> click(css(".introjs-nextbutton"))
+    |> click(css(".introjs-nextbutton"))
+    |> click(css(".introjs-nextbutton"))
+    |> click(css(".introjs-donebutton"))
+    |> visit("/home")
+    |> refute_has(css("#start-tour"))
+  end
+
+  feature "user interacts with intro js tour and dismisses it", %{session: session} do
+    session
+    |> set_cookie("show_welcome_modal", "")
+    |> visit("/home")
+    |> click(css("#start-tour"))
+    |> click(css(".introjs-skipbutton"))
+    |> visit("/")
+    |> find(Query.data("intro-show", "false"))
   end
 end
