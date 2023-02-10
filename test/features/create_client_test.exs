66c35d939aed63f6ca240ac9059123c67e4b1f33
Kyle Bacon <hello@kylebaconcreative.com>
2023-02-09 22:19:55 -0800

    Try this……………

--- test/features/create_client_test.exs
+++ test/features/create_client_test.exs
@@ -180,6 +180,7 @@ defmodule Picsello.CreateClientTest do
 
   defp open_add_client_popup(session) do
     session
+    |> set_cookie("show_welcome_modal", "")
     |> visit("/home")
     |> click(css("#hamburger-menu"))
     |> click(link("Clients"))
