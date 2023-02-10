d15549cfb81188cff64b0b242c7459518bfbd664
hrd-2 <hrd2@picsello.com>
2023-02-10 16:17:23 -0500

    fix order and testcases

--- test/features/create_client_test.exs
+++ test/features/create_client_test.exs
@@ -94,13 +94,8 @@ defmodule Picsello.CreateClientTest do
 
     session
     |> visit("/clients/#{client.id}/job-history")
-    |> click(link("Job Details"))
     |> click(css("a", text: "Go to Gallery"))
     |> assert_url_contains("/galleries")
-
-    assert Repo.all(Gallery) |> Enum.count() == 1
-
-    session
     |> visit("/clients/#{client.id}/job-history")
     |> find(css("[data-testid='client-jobs'] > div:first-child"), fn row ->
       row
