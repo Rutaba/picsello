b851c9c6e7460bbf1b6a3bd960c49418928446e9
hrd-2 <hrd2@picsello.com>
2023-02-16 00:22:14 -0500

    fix testcase

--- test/features/user_onboards_intro_test.exs
+++ test/features/user_onboards_intro_test.exs
@@ -55,10 +55,6 @@ defmodule Picsello.UserOnboardsIntroTest do
     |> click(css(".introjs-nextbutton"))
     |> click(css(".introjs-nextbutton"))
     |> click(css("a", text: "Next"))
-    |> find(css(".introjs-tooltipbuttons"), fn item -> 
-      item
-      |> click(css("a", text: "Next"))
-    end)
     |> click(css(".introjs-donebutton"))
     |> visit("/home")
     |> refute_has(css("#start-tour"))
