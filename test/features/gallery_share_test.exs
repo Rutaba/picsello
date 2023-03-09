723adcc918ce537272dd85f8ddcd6153dd603eee
hrd-2 <hrd2@picsello.com>
2023-03-09 23:44:04 +0500

    wip: testcase

--- test/features/gallery_share_test.exs
+++ test/features/gallery_share_test.exs
@@ -2,13 +2,14 @@ defmodule Picsello.GalleryShareTest do
   use Picsello.FeatureCase, async: true
   use Oban.Testing, repo: Picsello.Repo
 
+  alias Picsello.Repo
   alias Picsello.Galleries.PhotoProcessing.Waiter
 
   setup :onboarded
   setup :authenticated
   setup :authenticated_gallery
 
-  setup %{gallery: gallery} do
+  setup %{gallery: gallery, user: user} do
     Mox.stub(Picsello.PhotoStorageMock, :path_to_url, & &1)
     photo_ids = insert_photo(%{gallery: gallery, total_photos: 20})
 
@@ -21,6 +22,8 @@ defmodule Picsello.GalleryShareTest do
     )
 
     insert(:email_preset, type: :gallery, state: :gallery_send_link)
+
+    preload_some_clients(user) |> IO.inspect(label: "Inserted Clients ============>")
 
     [photo_ids: photo_ids]
   end
@@ -57,5 +60,56 @@ defmodule Picsello.GalleryShareTest do
     Waiter.complete_tracking(gallery.id, 1)
     assert_receive {:job_canceled, _}, 500
     assert_receive {:delivered_email, _}
+  end
+
+  feature "share gallery to multiple recipients", %{
+    session: session,
+    user: user,
+    gallery: gallery
+  } do
+    user |> IO.inspect()
+    # preload_some_clients(user)
+    # Repo.all(Picsello.Client)
+
+    session
+    |> visit("/galleries/#{gallery.id}")
+    |> click(button("Share gallery"))
+    |> within_modal(fn modal ->
+      modal
+      |> click(button("Add Cc"))
+      |> fill_in(text_field("cc_email"), with: "taylor@example.com; snow@example.com")
+      |> click(button("Add Bcc"))
+      |> fill_in(text_field("bcc_email"), with: "new")
+      |> assert_has(testid("bcc-error"))
+      |> click(button("remove-bcc"))
+      |> fill_in(text_field("search_phrase"), with: "client-1")
+      |> assert_has(css("#search_results"))
+      |> click(button("Add To"))
+      |> wait_for_enabled_submit_button(text: "Send Email")
+      |> click(button("Send Email"))
+    end)
+  end
+
+  defp preload_some_clients(user) do
+    insert(:client, %{
+      organization: user.organization,
+      name: "Elizabeth Taylor",
+      phone: "(210) 111-1234",
+      email: "taylor@example.com"
+    })
+
+    # insert(:client, %{
+    #   user: user,
+    #   name: "John Snow",
+    #   phone: "(241) 567-2352",
+    #   email: "snow@example.com"
+    # })
+
+    # insert(:client, %{
+    #   user: user,
+    #   name: "Michael Stark",
+    #   phone: "(442) 567-2321",
+    #   email: "stark@example.com"
+    # })
   end
 end
