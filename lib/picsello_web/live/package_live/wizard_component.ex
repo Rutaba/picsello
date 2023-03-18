9b28ee26e108bc55c8cd3ea6847311268591b66d
wasi <wasi@picsello.com>
2023-03-18 23:31:57 +0500

    working on package template

--- lib/picsello_web/live/package_live/wizard_component.ex
+++ lib/picsello_web/live/package_live/wizard_component.ex
@@ -492,22 +492,23 @@ defmodule PicselloWeb.PackageLive.WizardComponent do
         <div class={classes("p-4", %{"hidden" => Enum.member?(@collapsed_documents, 0)})}>
           <p>Here you can copy and paste your own contract or use the legally approved contract we have developed. You're also able to version your contract for different needs if you want!</p>
           <% c = form_for(@contract_changeset, "#") %>
-          <div class="grid grid-flow-col auto-cols-fr gap-4 mt-4">
-            <%= labeled_select c, :contract_template_id, @contract_options, label: "Select a Contract Template" %>
-            <%= labeled_input c, :name, label: "Contract Name", placeholder: "Enter new contract name", phx_debounce: "500" %>
-          </div>
-
-          <div class="flex justify-between items-end pb-2">
-            <label class="block mt-4 input-label" for={input_id(c, :content)}>Contract Language</label>
-            <%= cond do %>
-              <% !input_value(c, :contract_template_id) -> %>
-              <% input_value(c, :edited) -> %>
-                <.badge color={:blue}>Edited—new template will be saved</.badge>
-              <% !input_value(c, :edited) -> %>
-                <.badge color={:gray}>No edits made</.badge>
+          <div class="hidden sm:flex items-center justify-between border-b-8 border-blue-planning-300 font-semibold text-lg pb-3 mt-4 text-base-250">
+              <div class="w-1/3">Contract name</div>
+              <div class="w-1/3 text-center">Job type</div>
+              <div class="w-1/3 text-center">Select contract</div>
+            </div>
+            <%= for contract <- @contract_options do %>
+              <% checked = input_value(c, :contract_template_id) == contract.id %>
+              <div class={classes("border p-3 sm:py-4 sm:border-b sm:border-t-0 sm:border-x-0 rounded-lg sm:rounded-none border-gray-100", %{"bg-gray-100" => checked})}>
+                <label class="flex items-center justify-between cursor-pointer">
+                  <h3 class="font-xl font-bold w-1/3"><%= contract.name %></h3>
+                  <p class="w-1/3 text-center"><%= contract.job_type %></p>
+                  <div class="w-1/3 text-center">
+                    <%= radio_button(c, :contract_template_id, contract.id, class: "w-5 h-5 mr-2.5 radio") %>
+                  </div>
+                </label>
+              </div>
             <% end %>
-          </div>
-          <.quill_input f={c} id="quill_contract_input" html_field={:content} enable_size={true} track_quill_source={true} placeholder="Paste contract text here" />
         </div>
       </section>
       <section {testid("document-questionnaires")} class="border border-base-200 rounded-lg mt-4 overflow-hidden">
@@ -529,7 +530,7 @@ defmodule PicselloWeb.PackageLive.WizardComponent do
           <% else %>
             <div class="hidden sm:flex items-center justify-between border-b-8 border-blue-planning-300 font-semibold text-lg pb-3 mt-4 text-base-250">
               <div class="w-1/3">Questionnaire name</div>
-              <div class="w-1/3 text-center"># of questions</div>
+              <div class="w-1/3 text-center">Job type</div>
               <div class="w-1/3 text-center">Select questionnaire</div>
             </div>
             <%= for questionnaire <- @questionnaires do %>
@@ -537,7 +538,7 @@ defmodule PicselloWeb.PackageLive.WizardComponent do
               <div class={classes("border p-3 sm:py-4 sm:border-b sm:border-t-0 sm:border-x-0 rounded-lg sm:rounded-none border-gray-100", %{"bg-gray-100" => checked})}>
               <label class="flex items-center justify-between cursor-pointer">
                 <h3 class="font-xl font-bold w-1/3"><%= questionnaire.name %></h3>
-                <p class="w-1/3 text-center"><%= questionnaire.questions |> length()  %></p>
+                <p class="w-1/3 text-center"><%= questionnaire.job_type %></p>
                 <div class="w-1/3 text-center">
                   <%= radio_button(@f, :questionnaire_template_id, questionnaire.id, class: "w-5 h-5 mr-2.5 radio") %>
                 </div>
@@ -548,6 +549,12 @@ defmodule PicselloWeb.PackageLive.WizardComponent do
         </div>
       </section>
     """
+  end
+  def step(%{name: :documents1}=assigns) do
+    ~H"""
+      <section {testid("document-contracts")} class="border border-base-200 rounded-lg mt-4 overflow-hidden">
+      </section>
+      """
   end
 
   def step(%{name: :pricing} = assigns) do
@@ -883,8 +890,8 @@ defmodule PicselloWeb.PackageLive.WizardComponent do
         %{assigns: %{changeset: changeset}} = socket
       ) do
     template_id = template_id |> to_integer()
-
-    content =
+    IO.inspect(template_id, label: "template_id===>")
+    contract =
       case template_id do
         nil ->
           nil
@@ -894,17 +901,17 @@ defmodule PicselloWeb.PackageLive.WizardComponent do
 
           package
           |> Contracts.find_by!(id)
-          |> Contracts.contract_content(package, PicselloWeb.Helpers)
       end
 
     socket
-    |> assign_contract_changeset(%{"edited" => false})
-    |> push_event("quill:update", %{"html" => content})
+    |> assign_contract_changeset(%{"edited" => false, "contract_template_id" => template_id})
+    #|> push_event("quill:update", %{"html" => content})
     |> noreply()
   end
 
   @impl true
   def handle_event("validate", %{"contract" => contract} = params, socket) do
+    IO.inspect(params, label: "params validate ===> ")
     contract = contract |> Map.put_new("edited", Map.get(contract, "quill_source") == "user")
 
     params = params |> Map.put("contract", contract)
@@ -1514,6 +1521,7 @@ defmodule PicselloWeb.PackageLive.WizardComponent do
          params,
          action \\ nil
        ) do
+      IO.inspect(params, label: "params")
     global_settings =
       if global_settings,
         do: global_settings,
@@ -1593,12 +1601,21 @@ defmodule PicselloWeb.PackageLive.WizardComponent do
   end
 
   defp assign_contract_changeset(%{assigns: %{step: :documents}} = socket, params) do
+    IO.inspect(params, label: "params conract")
+    contract_template_id= Map.get(params, "contract_template_id")
     contract_params = Map.get(params, "contract", %{})
-
-    contract_changeset =
+    IO.inspect(contract_params, label: "contract_params")
+    contract = if contract_template_id do
+      socket.assigns.changeset
+      |> current()
+      |> Contracts.find_by!(contract_template_id)
+    else
       socket.assigns.changeset
       |> current()
       |> package_contract()
+    end
+    contract_changeset =
+      contract
       |> Contract.changeset(contract_params,
         skip_package_id: true,
         validate_unique_name_on_organization:
@@ -1607,7 +1624,7 @@ defmodule PicselloWeb.PackageLive.WizardComponent do
           )
       )
       |> Map.put(:action, :validate)
-
+      IO.inspect(contract_changeset, label: "contract_changeset===> ")
     socket |> assign(contract_changeset: contract_changeset)
   end
 
@@ -1621,16 +1638,20 @@ defmodule PicselloWeb.PackageLive.WizardComponent do
   defp validate_contract_name?(_), do: false
 
   defp assign_contract_options(%{assigns: %{step: :documents}} = socket) do
+    # options =
+    #   [
+    #     {"New Contract", ""}
+    #   ]
+    #   |> Enum.concat(
+    #     socket.assigns.changeset
+    #     |> current()
+    #     |> Contracts.for_package()
+    #     |> Enum.map(&{&1.name, &1.id})
+    #   )
     options =
-      [
-        {"New Contract", ""}
-      ]
-      |> Enum.concat(
         socket.assigns.changeset
         |> current()
         |> Contracts.for_package()
-        |> Enum.map(&{&1.name, &1.id})
-      )
 
     socket |> assign(contract_options: options)
   end
