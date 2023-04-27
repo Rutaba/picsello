2c828a86562ac83c72cf8a075eb9c01070202341
Ukasha Habib <ukasha@dycoders.com>
2023-04-27 11:19:22 +0500

    Fix some errors, organize & refactor code for global settings

--- lib/picsello/galleries.ex
+++ lib/picsello/galleries.ex
@@ -80,6 +80,7 @@ defmodule Picsello.Galleries do
       join: j in Job,
       on: j.id == g.job_id,
       join: c in Client,
+      as: :c,
       on: c.id == j.client_id,
       preload: [:albums, [job: :client]],
       where: c.organization_id == ^organization_id
@@ -1044,12 +1045,13 @@ defmodule Picsello.Galleries do
     |> if(do: :selections_available, else: uploading_status(gallery))
   end
 
-  def save_galleries_watermark(galleries, watermark_change) do
-    galleries
-    |> Enum.reduce(Multi.new(), fn %{id: id} = gallery, multi ->
-      Multi.run(multi, id, fn _, _ ->
-        save_gallery_watermark(gallery, watermark_change)
-      end)
+  def save_multiple_watermarks(galleries, watermark_change) do
+    Enum.reduce(galleries, Multi.new(), fn %{id: id} = gallery, multi ->
+      Multi.update(
+        multi,
+        id,
+        gallery_watermark_change(gallery, watermark_change)
+      )
     end)
     |> Repo.transaction()
   end
@@ -1060,18 +1062,18 @@ defmodule Picsello.Galleries do
   """
   def save_gallery_watermark(gallery, watermark_change) do
     gallery
-    |> Repo.preload(:watermark)
-    |> save_watermark(watermark_change)
+    |> gallery_watermark_change(watermark_change)
+    |> Repo.update()
     |> tap(fn
       {:ok, gallery} -> apply_watermark_on_photos(gallery)
       x -> x
     end)
   end
 
-  def save_watermark(gallery, watermark_change) do
+  defp gallery_watermark_change(gallery, watermark_change) do
     gallery
+    |> Repo.preload(:watermark)
     |> Gallery.save_watermark(watermark_change)
-    |> Repo.update()
   end
 
   def apply_watermark_on_photos(%{id: id} = gallery) do
@@ -1089,17 +1091,19 @@ defmodule Picsello.Galleries do
     Repo.preload(gallery, :watermark, force: true)
   end
 
-  def delete_multiple_watermarks(gallery_ids) do
-    clear_watermarks = fn multi, gallery_id ->
-      Multi.run(multi, gallery_id, fn _, _ -> clear_watermarks(gallery_id) end)
-    end
-
+  def delete_and_clear_multiple_watermarks(gallery_ids) do
     Multi.new()
     |> Multi.delete_all(
-      :delete_watermarks,
+      :delete_all_watermarks,
       from(w in Watermark, where: w.gallery_id in ^gallery_ids)
     )
-    |> then(fn m -> Enum.reduce(gallery_ids, m, &clear_watermarks.(&2, &1)) end)
+    |> then(fn multi ->
+      Enum.reduce(
+        gallery_ids,
+        multi,
+        &Multi.run(&2, &1, fn _, _ -> clear_watermarks(&1, :no) end)
+      )
+    end)
     |> Repo.transaction()
   end
 
@@ -1113,20 +1117,19 @@ defmodule Picsello.Galleries do
   @doc """
   Clears watermarks of photos and triggers watermarked versions removal
   """
-  def clear_watermarks(gallery_id) do
-    %{job: %{client: %{organization: %{name: name}}}} =
-      gallery = get_gallery!(gallery_id) |> populate_organization()
+  def clear_watermarks(gallery_id, apply_watermark_to_proofing? \\ :yes) do
+    gallery = get_gallery!(gallery_id)
 
-    gallery
-    |> Repo.preload(photos: [:album])
-    |> Map.get(:photos)
-    |> Enum.reduce({[], []}, fn
-      %{album: %{is_proofing: true}} = photo, acc ->
-        ProcessingManager.start(photo, Watermark.build(name, gallery))
-        acc
+    gallery_id
+    |> get_gallery_photos()
+    |> Repo.preload(:album)
+    |> Enum.reduce({[], [], []}, fn
+      %{album: %{is_proofing: true}} = photo, {proofing_photos, photo_ids, oban_jobs} ->
+        {[photo | proofing_photos], photo_ids, oban_jobs}
 
-      photo, {photo_ids, oban_jobs} ->
+      photo, {proofing_photos, photo_ids, oban_jobs} ->
         {
+          proofing_photos,
           [photo.id | photo_ids],
           [photo.watermarked_preview_url, photo.watermarked_url]
           |> Enum.map(fn path -> CleanStore.new(%{path: path}) end)
@@ -1134,10 +1137,7 @@ defmodule Picsello.Galleries do
         }
     end)
     |> then(fn
-      {[], []} ->
-        {:ok, %{}}
-
-      {photo_ids, oban_jobs} ->
+      {proofing_photos, photo_ids, oban_jobs} ->
         Multi.new()
         |> Multi.update_all(
           :photos,
@@ -1150,8 +1150,26 @@ defmodule Picsello.Galleries do
           []
         )
         |> Oban.insert_all(:clean_store, oban_jobs)
+        |> Multi.put(:proofing_photos, proofing_photos)
         |> Repo.transaction()
     end)
+    |> tap(fn
+      {:ok, %{proofing_photos: proofing_photos}} = result
+      when apply_watermark_to_proofing? == :yes ->
+        apply_watermark_to_photos(proofing_photos, gallery)
+
+        result
+
+      result ->
+        result
+    end)
+  end
+
+  def apply_watermark_to_photos(photos, gallery) do
+    %{job: %{client: %{organization: %{name: name}}}} = populate_organization(gallery)
+    watermark = Watermark.build(name, gallery)
+
+    Enum.each(photos, &ProcessingManager.start(&1, watermark))
   end
 
   def gallery_password_change(attrs \\ %{}) do
