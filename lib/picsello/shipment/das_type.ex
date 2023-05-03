1555e86f15ff7f93052a2175d8675d893be6936c
Ukasha Habib <ukasha@dycoders.com>
2023-05-03 16:16:03 +0500

    Add schemas for shipments

--- lib/picsello/shipment/das_type.ex
+++ lib/picsello/shipment/das_type.ex
@@ -1,15 +1,10 @@
 defmodule Picsello.Shipment.DasType do
-  @moduledoc false
   use Ecto.Schema
   import Ecto.Changeset
 
-  alias Picsello.Shipment.{DasType, Zipcode}
-  alias Picsello.Repo
-  import Ecto.Query
-
   schema "shipment_das_types" do
-    field :mail_cost, Money.Ecto.Amount.Type
-    field :parcel_cost, Money.Ecto.Amount.Type
+    field :mail_cost, :decimal
+    field :parcel_cost, :decimal
     field :name, :string
 
     timestamps()
@@ -21,15 +16,5 @@ defmodule Picsello.Shipment.DasType do
     das_cost
     |> cast(attrs, @fields)
     |> validate_required(@fields)
-  end
-
-  def get_by_zipcode(zipcode) do
-    from(dt in DasType,
-      join: zipcode in Zipcode,
-      on: dt.id == zipcode.das_type_id,
-      where: zipcode.zipcode == ^zipcode,
-      limit: 1
-    )
-    |> Repo.one()
   end
 end
--- lib/picsello/shipment/detail.ex
+++ lib/picsello/shipment/detail.ex
@@ -1,27 +1,22 @@
 defmodule Picsello.Shipment.Detail do
-  @moduledoc false
   use Ecto.Schema
   import Ecto.Changeset
-  alias Picsello.Repo
 
   schema "shipment_details" do
-    field :base_charge, Money.Ecto.Amount.Type
+    field :base_charge, :decimal
     field :das_carrier, Ecto.Enum, values: [:mail, :parcel]
     field :order_attribute_id, :integer
-    field(:type, Ecto.Enum, values: [:economy_usps, :economy_trackable, :three_days, :one_day])
-
+    field :type, :string
     field :upcharge, :map
 
     timestamps()
   end
 
-  @fields ~w(type base_charge order_attribute_id das upcharge)a
+  @fields ~w(type base_charge order_attribute_id das upcharge)
   @doc false
   def changeset(shipment_detail, attrs) do
     shipment_detail
     |> cast(attrs, @fields)
     |> validate_required(@fields -- [:upcharge])
   end
-
-  def all(), do: Repo.all(__MODULE__)
 end
