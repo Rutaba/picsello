5677cf474a9ec5341164de372ebb95f9da3e7cbd
Ukasha Habib <ukasha@dycoders.com>
2023-05-03 16:16:03 +0500

    Add schemas for shipments

---
+++ lib/picsello/shipment/detail.ex
@@ -0,0 +1,22 @@
+defmodule Picsello.Shipment.Detail do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "shipment_details" do
+    field :base_charge, :decimal
+    field :das_carrier, Ecto.Enum, values: [:mail, :parcel]
+    field :order_attribute_id, :integer
+    field :type, :string
+    field :upcharge, :map
+
+    timestamps()
+  end
+
+  @fields ~w(type base_charge order_attribute_id das upcharge)
+  @doc false
+  def changeset(shipment_detail, attrs) do
+    shipment_detail
+    |> cast(attrs, @fields)
+    |> validate_required(@fields -- [:upcharge])
+  end
+end
