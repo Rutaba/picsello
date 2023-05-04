4ad6a73e6ff80634f48daa4db96a6deea9293c28
Ukasha Habib <ukasha@dycoders.com>
2023-05-04 10:15:58 +0500

    Update product configs

--- config/config.exs
+++ config/config.exs
@@ -119,16 +119,9 @@ config :picsello, :whcc,
 
 config :picsello, :products,
   whcc_album_id: "2qNgr3zcSx9wvTAo9",
+  whcc_wall_art_id: "tfhysKwZafFtmGqpQ",
   whcc_books_id: "B9FcAHDH5T63yvvgX",
-  whcc_photo_prints_id: "BBrgfCJLkGzseCdds",
-  base_charges: [
-    economy_usps: [value: 3.8, uuid: 545],
-    economy_trackable: [value: 3.8, uuid: 546],
-    three_days: [value: 11.95, uuid: 100],
-    one_day: [value: 22.95, uuid: 1728],
-    album_flat_rate: [value: 7.50, uuid: 548],
-    book_flat_rate: [value: 7.50, uuid: 553]
-  ]
+  whcc_photo_prints_id: "BBrgfCJLkGzseCdds"
 
 config :picsello, Oban,
   repo: Picsello.Repo,
