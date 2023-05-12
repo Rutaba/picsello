b4ab6079725bfaaafa9655f522a0ced3a9e7eaa1
wasi <wasi@picsello.com>
2023-05-12 21:31:37 +0500

    fixed expiration date redirection

--- lib/picsello_web/live/gallery_live/index.ex
+++ lib/picsello_web/live/gallery_live/index.ex
@@ -21,13 +21,14 @@ defmodule PicselloWeb.GalleryLive.Index do
 
   @impl true
   def mount(
-        _params,
+        params,
         _session,
         socket
       ) do
     socket
     |> assign_new(:pagination, fn -> %Pagination{} end)
     |> update_gallery_listing()
+    |> is_mobile(params)
     |> ok()
   end
 
@@ -365,8 +366,9 @@ defmodule PicselloWeb.GalleryLive.Index do
 
     icon_text_class =
       if icon in ["trash", "closed-eye"], do: "text-red-sales-300", else: "text-blue-planning-300"
+
     assigns = assign(assigns, icon_text_class: icon_text_class)
-    
+
     ~H"""
     <a {@link} class={"text-gray-700 block px-4 py-2 text-sm hover:bg-blue-planning-100 #{@class}"} role="menuitem" tabindex="-1" id={@id} }>
       <.icon name={@icon} class={"w-4 h-4 fill-current #{@icon_text_class} inline mr-1"} />
