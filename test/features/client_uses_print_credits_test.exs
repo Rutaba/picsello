0c1d24ee21dcdfd867749f1508781c4f625a935d
Ukasha Habib <ukasha@dycoders.com>
2023-05-17 21:21:56 +0500

    Print credits test cases

--- test/features/client_uses_print_credits_test.exs
+++ test/features/client_uses_print_credits_test.exs
@@ -185,8 +185,6 @@ defmodule Picsello.ClientUsesPrintCreditsTest do
   end
 
   def expect_create_invoice(%{stripe_invoice: %{amount_due: amount} = invoice}) do
-    IO.inspect(amount)
-
     Picsello.MockPayments
     |> Mox.expect(:create_invoice, fn %{
                                         auto_advance: true,
@@ -196,11 +194,10 @@ defmodule Picsello.ClientUsesPrintCreditsTest do
       {:ok, invoice}
     end)
     |> Mox.expect(:create_invoice_item, fn %{
-                                             amount: amount,
+                                             amount: ^amount,
                                              customer: "photographer-stripe-customer-id"
                                            },
                                            _opts ->
-      IO.inspect(amount)
       {:ok, %Stripe.Invoiceitem{invoice: invoice}}
     end)
 
@@ -233,8 +230,6 @@ defmodule Picsello.ClientUsesPrintCreditsTest do
 
     Picsello.MockPayments
     |> Mox.expect(:create_session, fn params, _ ->
-      IO.inspect(params)
-
       assert %{
                success_url: success_url,
                payment_intent_data: %{
@@ -307,13 +302,13 @@ defmodule Picsello.ClientUsesPrintCreditsTest do
           build(:stripe_invoice,
             id: "stripe-invoice-id",
             description: "stripe invoice!",
-            amount_due: 4510,
+            amount_due: 5000,
             amount_remaining: 5000,
             status: :draft
           ),
         whcc_unit_base_price: ~M[4200]USD,
         whcc_total: ~M[5000]USD,
-        stripe_checkout: %{application_fee_amount: ~M[490]USD, amount: ~M[5500]USD}
+        stripe_checkout: %{application_fee_amount: ~M[1095]USD, amount: ~M[5500]USD}
       ]
     end
 
@@ -334,7 +329,7 @@ defmodule Picsello.ClientUsesPrintCreditsTest do
       |> assert_text("Cart & Shipping Review")
       |> assert_has(definition("Products (1)", text: "$4,242.00"))
       |> assert_has(definition("Print credits used", text: "$4,242.00"))
-      |> assert_has(definition("Total", text: "$4.90"))
+      |> assert_has(definition("Total", text: "$10.95"))
       |> click(link("Continue"))
       |> fill_in_shipping()
       |> click(button("Check out"))
@@ -344,7 +339,7 @@ defmodule Picsello.ClientUsesPrintCreditsTest do
       session
       |> assert_url_contains("orders")
       |> assert_text("Order details")
-      |> assert_has(definition("Total", text: "$4.90"))
+      |> assert_has(definition("Total", text: "$10.95"))
       |> assert_has(definition("Print credits used", text: "$4,242.00"))
       |> click(link("Home"))
       |> assert_has(definition("Print Credit", text: "$758.00"))
@@ -377,7 +372,7 @@ defmodule Picsello.ClientUsesPrintCreditsTest do
           build(:stripe_invoice,
             id: "stripe-invoice-id",
             description: "stripe invoice!",
-            amount_due: 4510,
+            amount_due: 5000,
             amount_remaining: 5000,
             status: :draft
           ),
