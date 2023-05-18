a391c3866229f17e364b9f5ac0be78c6998ebfbe
Ukasha Habib <ukasha@dycoders.com>
2023-05-18 09:47:13 -0400

    remove shipping upcharge migration

--- priv/repo/migrations/20230503073889_remove_shipping_upcharge_from_products.exs
+++
@@ -1,9 +0,0 @@
-defmodule Picsello.Repo.Migrations.RemoveShippingUpchargeFromProducts do
-  use Ecto.Migration
-
-  def change do
-    alter table(:products) do
-      remove(:shipping_upcharge)
-    end
-  end
-end
