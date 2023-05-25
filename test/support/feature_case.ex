9bea2327ea7cc2f9627452d28feda5ec7b15360a
Ukasha Habib <ukasha@dycoders.com>
2023-05-25 10:57:35 -0400

    attempt to pass the login disabled button

--- test/support/feature_case.ex
+++ test/support/feature_case.ex
@@ -124,7 +124,7 @@ defmodule Picsello.FeatureCase do
         session |> assert_has(css("button:not(:disabled)[type='submit']", opts))
       rescue
         _ ->
-          session 
+          session
           |> sleep(500)
           |> scroll_to_bottom()
           |> find(css("button:not(:disabled)[type='submit']", opts), & &1)
@@ -165,6 +165,7 @@ defmodule Picsello.FeatureCase do
       |> fill_in(text_field("Email"), with: email)
       |> fill_in(text_field("Password"), with: password)
       |> wait_for_enabled_submit_button()
+      |> sleep(500)
       |> click(button("Login"))
       |> then(&wait_for_path_to_change_from(&1, @sign_in_path))
     end
