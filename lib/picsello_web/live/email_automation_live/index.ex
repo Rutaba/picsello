ef2f54197e2ad6956428313c71b03d19e4fee16e
Abdul <abdul@picsello.com>
2023-06-01 16:54:01 +0500

    Email automations page is designed.

--- lib/picsello_web/live/email_automation_live/index.ex
+++ lib/picsello_web/live/email_automation_live/index.ex
@@ -4,6 +4,7 @@ defmodule PicselloWeb.Live.EmailAutomations.Index do
   import PicselloWeb.Live.Calendar.Shared, only: [back_button: 1]
   import Picsello.Onboardings, only: [save_intro_state: 3]
   alias Ecto.Changeset
+  import PicselloWeb.LiveHelpers
 
   alias Picsello.{
     Repo,
@@ -14,6 +15,8 @@ defmodule PicselloWeb.Live.EmailAutomations.Index do
   def mount(_params, _session, socket) do
     socket
     |> assign(:page_title, "Automations")
+    |> assign(type: "Default")
+    |> assign(:collapsed_sections, [])
     |> default_assigns()
     |> ok()
   end
@@ -33,5 +36,32 @@ defmodule PicselloWeb.Live.EmailAutomations.Index do
       :job_types,
       Profiles.enabled_job_types(current_user.organization.organization_job_types)
     )
+  end
+
+  def handle_event(
+        "assign_templates_by_type",
+        %{"type" => type},
+         socket
+      ) do
+    socket
+    |> assign(:type, type)
+    |> noreply()
+  end
+
+  def handle_event(
+      "toggle-section",
+      %{"section_id" => section_id},
+      %{assigns: %{collapsed_sections: collapsed_sections}} = socket
+    ) do
+  collapsed_sections =
+    if Enum.member?(collapsed_sections, section_id) do
+      Enum.filter(collapsed_sections, &(&1 != section_id))
+    else
+      collapsed_sections ++ [section_id]
+    end
+
+    socket
+    |> assign(:collapsed_sections, collapsed_sections)
+    |> noreply()
   end
 end
