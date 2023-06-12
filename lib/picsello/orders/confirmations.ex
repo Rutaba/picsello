a98424fbd86d55780791623cd93b1f16d5e9fd9f
Ukasha Habib <ukasha@dycoders.com>
2023-06-12 16:01:21 -0400

    add print credits in photographer owes

--- lib/picsello/orders/confirmations.ex
+++ lib/picsello/orders/confirmations.ex
@@ -191,9 +191,12 @@ defmodule Picsello.Orders.Confirmations do
 
   defp photographer_owes(_repo, %{
          intent: %{application_fee_amount: application_fee_amount, amount: amount},
-         order: order
+         order: %{products: products} = order
        }) do
-    print_cost = application_fee_amount |> Money.add(stripe_processing_fee(order))
+
+    print_credits = products |> Enum.reduce(~M[0]USD, &Money.add(&2, &1.print_credit_discount))
+    print_cost = application_fee_amount |> Money.add(stripe_processing_fee(order)) |> Money.add(print_credits)
+
 
     case Money.cmp(amount, print_cost) do
           :lt -> {:ok, Money.subtract(print_cost, amount)}
