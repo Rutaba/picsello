de0a0f255c68553c11f2fb805d1700d621292094
Ukasha Habib <ukasha@dycoders.com>
2023-07-05 16:31:59 +0500

    Change use_global default value in gallery

---
+++ priv/repo/migrations/30230330153865_change_use_global_default_value.exs
@@ -0,0 +1,11 @@
+defmodule Picsello.Repo.Migrations.ChangeUseGlobalDefaultValue do
+  use Ecto.Migration
+
+  @default %{products: true}
+
+  def change do
+    alter table(:galleries) do
+      modify(:use_global, :map, default: @default)
+    end
+  end
+end
