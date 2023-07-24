2d1a615f1898b0385cd546975ca6ef04a443c404
Abdul <abdul@picsello.com>
2023-07-24 18:02:01 +0500

    fix test case

--- test/features/user_onboards_intro_test.exs
+++ test/features/user_onboards_intro_test.exs
@@ -10,31 +10,17 @@ defmodule Picsello.UserOnboardsIntroTest do
   setup :onboarded_show_intro
   setup :authenticated
 
-  feature "user gets welcome modal, clicks client booking", %{session: session} do
+  feature "user gets welcome modal", %{session: session} do
     session
-    |> assert_text("Welcome to the Picsello Family!")
-    |> click(css(".welcome-column", count: 3, at: 0))
-    |> assert_path("/booking-events")
-    |> assert_text("Booking events")
-    |> visit("/home")
-    |> assert_text("To do")
+    |> assert_text("Welcome to Picsello!")
+    |> assert_text("Let's do it")
+    |> assert_text("Not yet, I want to play around first")
   end
 
-  feature "user gets welcome modal, clicks galleries", %{session: session} do
+  feature "user gets welcome modal, clicks I want to play", %{session: session} do
     session
-    |> assert_text("Welcome to the Picsello Family!")
-    |> click(css(".welcome-column", count: 3, at: 1))
-    |> assert_path("/galleries")
-    |> assert_text("Your Galleries")
-    |> visit("/home")
-    |> assert_text("To do")
-  end
-
-  feature "user gets welcome modal, clicks demo", %{session: session} do
-    session
-    |> assert_text("Welcome to the Picsello Family!")
-    |> click(link("Join a demo"))
-    |> visit("/home")
-    |> refute_has(css("#welcome-text", text: "Welcome to the Picsello Family!"))
+    |> assert_text("Welcome to Picsello!")
+    |> click(button("Not yet, I want to play around first"))
+    |> refute_has(css("#welcome-text", text: "Welcome to Picsello!"))
   end
 end
