f6e64d7fc690b859d0c14d17c58ba541b50835ca
wasi <wasi@dycoders.com>
2023-08-15 17:40:55 +0500

    PIC-1982 Update contact form

--- lib/picsello_web/live/profile/client_form_component.ex
+++ lib/picsello_web/live/profile/client_form_component.ex
@@ -18,25 +18,25 @@ defmodule PicselloWeb.Live.Profile.ClientFormComponent do
 
     ~H"""
     <div class="mt-20 border p-9 border-base-200">
-      <h2 class="text-3xl font-light max-w-md">Get in touch<%= @header_suffix %></h2>
+      <h2 class="text-3xl max-w-md">Let's work together!<%= @header_suffix %></h2>
 
       <%= if @changeset do %>
         <.form for={@changeset} :let={f} phx-change="validate-client" phx-submit="save-client" id="contact-form" phx-target={@myself}>
           <div class="flex flex-col mt-3">
-            <%= label_for f, :name, autocapitalize: "words", autocorrect: "false", spellcheck: "false", autocomplete: "name", label: "Your name", class: "py-2 font-light" %>
+            <%= label_for f, :name, autocapitalize: "words", autocorrect: "false", spellcheck: "false", autocomplete: "name", label: "Name", class: "py-2 font-bold" %>
 
             <%= input f, :name, placeholder: "Type your first and last name...", phx_debounce: 300 %>
           </div>
 
           <div class="flex flex-col lg:flex-row">
             <div class="flex flex-col flex-1 mt-3 mr-0 lg:mr-4">
-              <%= label_for f, :email, label: "Your email", class: "py-2 font-light" %>
+              <%= label_for f, :email, label: "Email", class: "py-2 font-bold" %>
 
               <%= input f, :email, type: :email_input, placeholder: "Type email...", phx_debounce: 300 %>
             </div>
 
             <div class="flex flex-col flex-1 mt-3">
-              <%= label_for f, :phone, label: "Your phone number", class: "py-2 font-light" %>
+              <%= label_for f, :phone, label: "Phone Number", class: "py-2 font-bold" %>
 
               <%= input f, :phone, type: :telephone_input, placeholder: "Type phone number...", phx_debounce: 300 %>
             </div>
@@ -57,20 +57,19 @@ defmodule PicselloWeb.Live.Profile.ClientFormComponent do
               </div>
             <% end %>
           <%= if Enum.any?(@job_types) do %>
-            <div class="mt-4">
-              <%= label_for f, :job_type, label: "What type of session are you looking for?", class: "font-light" %>
-              <div class="grid grid-cols-2 lg:grid-cols-3 gap-4 mt-1">
-                <%= for job_type <- @job_types do %>
-                  <.job_type_option name={input_name(f, :job_type)} type={:radio} job_type={job_type} checked={input_value(f, :job_type) == job_type} color="black" class="rounded-none" />
-                <% end %>
-              </div>
+            <div class="mt-7 grid grid-cols-1 lg:grid-cols-2 gap-4">
+              <%= label_for f, :job_type, label: "What type of session you are looking for?", class: "py-2 font-bold col-span-1 lg:col-span-2" %>
+
+              <%= for job_type <- @job_types do %>
+                <.job_type_option name={input_name(f, :job_type)} type={:radio} job_type={job_type} checked={input_value(f, :job_type) == job_type} color="black" class="rounded-none" />
+              <% end %>
             </div>
           <% end %>
 
           <div class="flex flex-col mt-3">
             <%= label_for f, :message, label: "Your message", class: "py-2 font-light" %>
 
-            <%= input f, :message, type: :textarea, placeholder: "Type your message...", rows: 5, phx_debounce: 300 %>
+            <%= input f, :message, type: :textarea, placeholder: "Tell me more about what you are looking for. I love details!", rows: 5, phx_debounce: 300 %>
           </div>
 
           <div class="mt-8 text-right"><button type="submit" disabled={!@changeset.valid?} class="w-full lg:w-auto btn-primary">Submit</button></div>
