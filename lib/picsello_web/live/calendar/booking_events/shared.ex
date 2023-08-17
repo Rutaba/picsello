cc251bb628d655fd6ee0d278072315a321c3dcec
Ukasha <ukasha@dycoders.com>
2023-08-17 17:31:55 +0500

    helper file for booking events

---
+++ lib/picsello_web/live/calendar/booking_events/shared.ex
@@ -0,0 +1,13 @@
+defmodule PicselloWeb.Calendar.BookingEvents.Shared do
+
+
+  # tells us if the created/duplicated booking event is complete or not
+  # if we dont have dates or a package_template_id, then its incomplete
+  # similarly its complete if both dates and package_template_id exist
+  def incomplete_status?(%{package_template_id: nil}), do: true
+  def incomplete_status?(%{dates: []}), do: true
+  def incomplete_status?(_), do: false
+
+  # will be true if the status matches in the array <status_list>
+  def disabled?(booking_event, status_list), do: booking_event.status in status_list
+end
