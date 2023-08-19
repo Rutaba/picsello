fb692ef8bec24d6248f77baffe834fab30238059
Kyle Bacon <hello@kylebaconcreative.com>
2023-08-19 16:08:55 -0700

    Update printable invoice, had header/footer partials

--- lib/picsello_web/controllers/job_download_controller.ex
+++ lib/picsello_web/controllers/job_download_controller.ex
@@ -2,7 +2,8 @@ defmodule PicselloWeb.JobDownloadController do
   use PicselloWeb, :controller
 
   import PicselloWeb.BookingProposalLive.Shared, only: [get_print_credit: 1, get_amount: 1]
-  alias Picsello.BookingProposal
+  import Picsello.Profiles, only: [logo_url: 1]
+  alias Picsello.{Repo, BookingProposal, Contracts}
 
   def download_invoice_pdf(conn, %{"booking_proposal_id" => booking_proposal_id}) do
     %{
@@ -10,16 +11,19 @@ defmodule PicselloWeb.JobDownloadController do
         %{
           client: client,
           shoots: shoots,
-          package: %{organization: %{user: photographer} = organization} = package
+          package:
+            %{contract: contract, organization: %{user: photographer} = organization} = package
         } = job
     } =
       proposal =
       booking_proposal_id
       |> BookingProposal.by_id()
       |> BookingProposal.preloads()
+      |> Repo.preload(job: [package: [:contract]])
 
     print_credit = get_print_credit(package)
     amount = get_amount(print_credit)
+    organization_logo_url = logo_url(organization)
 
     PicselloWeb.PDFView.render("job_invoice.html", %{
       read_only: true,
@@ -30,12 +34,26 @@ defmodule PicselloWeb.JobDownloadController do
       client: client,
       shoots: shoots,
       package: package,
+      contract: contract,
+      contract_content:
+        Contracts.contract_content(
+          contract,
+          package,
+          PicselloWeb.Helpers
+        ),
       print_credit: print_credit,
       amount: amount
     })
     |> Phoenix.HTML.Safe.to_iodata()
     |> IO.iodata_to_binary()
-    |> PdfGenerator.generate(page_size: "A5")
+    |> PdfGenerator.generate(
+      page_size: "A5",
+      shell_params:
+        generate_pdf_options(%{
+          organization: organization,
+          organization_logo_url: organization_logo_url
+        })
+    )
     |> then(fn {:ok, path} ->
       conn
       |> put_resp_content_type("pdf")
@@ -45,5 +63,25 @@ defmodule PicselloWeb.JobDownloadController do
       )
       |> send_resp(200, File.read!(path))
     end)
+  end
+
+  defp generate_pdf_options(params) do
+    header = render_page_wrapper("client_header.html", params)
+    footer = render_page_wrapper("client_footer.html", params)
+
+    ["--header-html", "file://#{header}", "--footer-html", "file://#{footer}"]
+  end
+
+  defp render_page_wrapper(template, params) do
+    tmp_dir = System.tmp_dir!()
+    location = Path.join(tmp_dir, template)
+
+    File.write!(
+      location,
+      PicselloWeb.PDFView.render(template, params)
+      |> Phoenix.HTML.Safe.to_iodata()
+    )
+
+    location
   end
 end
--- lib/picsello_web/views/pdf_view.ex
+++ lib/picsello_web/views/pdf_view.ex
@@ -1,3 +1,7 @@
 defmodule PicselloWeb.PDFView do
   use PicselloWeb, :view
+  alias Picsello.{PaymentSchedules}
+
+  import PicselloWeb.BookingProposalLive.ScheduleComponent,
+    only: [make_status: 1, status_class: 1]
 end
