f6e47dddff602b3429fc7f842e836e5682eede1d
Kyle Bacon <hello@kylebaconcreative.com>
2023-08-23 11:18:03 -0700

    Update js

--- assets/js/hooks/check-idle.js
+++ assets/js/hooks/check-idle.js
@@ -1,19 +1,33 @@
+let time;
+
 export default {
   checkInactivity(idleTimeout, component) {
-    let time;
     window.onload = resetTimer;
     document.onmousemove = resetTimer;
     document.onkeypress = resetTimer;
+
     function handlePopup() {
       component.pushEvent('fire_idle_popup', {});
     }
+
     function resetTimer() {
-      clearTimeout(time);
+      clearTimer();
       time = setTimeout(handlePopup, idleTimeout);
+    }
+
+    function clearTimer() {
+      console.log(time);
+      clearTimeout(time);
     }
   },
   mounted() {
     const { idleTimeout } = this.el.dataset;
     this.checkInactivity(parseInt(idleTimeout || 180000), this);
+  },
+  destroyed() {
+    window.onload = null;
+    document.onmousemove = null;
+    document.onkeypress = null;
+    clearTimeout(time);
   },
 };
