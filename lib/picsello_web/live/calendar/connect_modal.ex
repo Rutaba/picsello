f6fcdd7c7d604903859b306d1339e547ed9b217d
aqib <aqib@dycoder.com>
2023-08-29 02:21:23 +0500

    Changes to settings view of calendar

--- lib/picsello_web/live/calendar/connect_modal.ex
+++ lib/picsello_web/live/calendar/connect_modal.ex
@@ -14,20 +14,19 @@ defmodule PicselloWeb.Live.Calendar.Shared.ConnectModal do
     ~H"""
     <div class="dialog">
       <h1 class="flex justify-between mb-4 text-3xl font-bold">
-        Connect calendar
+        2-way calendar sync
         <button phx-click="modal" phx-value-action="close" title="close modal" type="button" class="p-2">
           <.icon name="close-x" class="w-4 h-4 stroke-current stroke-2"/>
         </button>
       </h1>
       <img src="/images/calendar-sync-smaller.jpg" />
-      <p class="font-bold mt-4">Securely connect your calendar so we can:</p>
+      <p class="font-bold mt-4">Securely connect your external and Picsello calendars so you can:</p>
       <ul class="list-disc ml-4">
-        <li>Create new events from Picsello actions (bookings and shoots)</li>
-        <li>Delete Events created by Picsello</li>
-        <li>Read Events from Calendars you've authorized to have singular view of your business in one spot</li>
+        <li>Avoid schedule conflicts and double-bookings between your photography and personal calendars</li>
+        <li>Have all your external calendar events and details will synced with your Picsello account and vice versa!</li>
       </ul>
       <PicselloWeb.LiveModal.footer class="pt-8">
-        <a class="btn-primary" id="button-connect" href={@nylas_url}>Connect Calendar</a>
+        <a class="btn-primary" id="button-connect" href={@nylas_url}>Sync calendars</a>
         <button class="btn-secondary" title="cancel" type="button" phx-click="modal" phx-value-action="close">
           Close
         </button>
