f89d35fdf88d8d7166ae132dfb5ee4e9182298bf
aqib <aqib@dycoder.com>
2023-09-07 15:26:35 +0500

    Change behaviour of refresh calendar view

--- assets/js/hooks/calendar.js
+++ assets/js/hooks/calendar.js
@@ -2,6 +2,7 @@ import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
+import Cookies from 'js-cookie';
 
 const isMobile = () => window.innerWidth <= 768;
 
@@ -12,12 +13,15 @@ const getView = () => {
 const calendar_render = (component, el) => {
   const { timeZone, feedPath } = el.dataset;
 
+  const date = Cookies.get('c_date');
+  const view = Cookies.get('c_view');
+
   const calendar = new Calendar(el, {
     height: 'auto',
     plugins: [dayGridPlugin, listPlugin, timeGridPlugin],
     timeZone,
     height: 'auto',
-    initialView: getView(),
+    initialView: view || getView(),
     headerToolbar: {
       left: 'prev,next today',
       center: 'title',
@@ -26,6 +30,21 @@ const calendar_render = (component, el) => {
     eventBackgroundColor: 'green',
     eventBorderColor: 'green',
     eventColor: 'green',
+    initialDate: date,
+    datesSet: function (info) {
+      var date = info.startStr
+      if (info.view.type == "dayGridMonth") {
+        date = new Date(info.startStr)
+        date.setDate(date.getDate() + 10)
+        date = date.toISOString().slice(0, -5);
+      }
+
+      const dateTime = new Date();
+      dateTime.setMinutes(dateTime.getMinutes() + 10);
+
+      Cookies.set('c_date', date, { expires: dateTime, path: '/', })
+      Cookies.set('c_view', info.view.type, { expires: dateTime, path: '/', })
+    },
     eventSources: [{ url: feedPath }],
     eventClick: function (info) {
       component.pushEvent('event-detail', { event: info.event })
