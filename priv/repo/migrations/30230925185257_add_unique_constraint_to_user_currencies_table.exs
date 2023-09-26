5ee0aee8e7d273490a504b58187b8b3fbeaaca7b
aqib008 <aqib@dycoders.com>
2023-09-26 15:45:18 +0500

    Minor fix to migartion for unique index of user currencies

--- priv/repo/migrations/30230925185257_add_unique_constraint_to_user_currencies_table.exs
+++
@@ -1,7 +0,0 @@
-defmodule Picsello.Repo.Migrations.AddUniqueConstraintToUserCurrenciesTable do
-  use Ecto.Migration
-
-  def change do
-    create(unique_index(:user_currencies, [:organization_id]))
-  end
-end
