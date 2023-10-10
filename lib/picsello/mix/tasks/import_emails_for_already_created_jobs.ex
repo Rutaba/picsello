2ff025eb1eeb46635cd1266838aa8e906b3e9f48
hrd-2 <hrd2@picsello.com>
2023-10-10 23:10:07 +0500

    Prevent some-emails when reminded_at is nil for some schedules

--- lib/picsello/mix/tasks/import_emails_for_already_created_jobs.ex
+++ lib/picsello/mix/tasks/import_emails_for_already_created_jobs.ex
@@ -67,10 +67,20 @@ defmodule Mix.Tasks.ImportEmailForAlreadyCreatedJobs do
 
   defp jobs_emails_insert(jobs, org_id) do
     Enum.map(jobs, fn job ->
-      insert_email_schedules_job(job.type, job.id, org_id, [:job])
+      skip_states =
+        if is_skip_states?(job.payment_schedules),
+          do: [""],
+          else: [:balance_due, :offline_payment, :paid_full, :paid_offline_full]
+
+      insert_email_schedules_job(job.type, job.id, org_id, [:job], skip_states)
       galleries_emails(job.galleries)
     end)
   end
+
+  defp is_skip_states?(payment_schedules),
+    do:
+      payment_schedules
+      |> Enum.any?(&is_nil(&1.reminded_at))
 
   defp galleries_emails(galleries) do
     galleries
@@ -92,13 +102,13 @@ defmodule Mix.Tasks.ImportEmailForAlreadyCreatedJobs do
          job_id,
          organization_id,
          categories,
-         skip_pipelines \\ [""]
+         skip_pipelines
        ) do
     Shared.insert_job_emails(job_type, organization_id, job_id, categories, skip_pipelines)
   end
 
   defp filter_jobs(query) do
-    from(j in query, preload: [:job_status, galleries: :orders])
+    from(j in query, preload: [:job_status, :payment_schedules, galleries: :orders])
     |> Repo.all()
     |> Enum.filter(&(is_nil(&1.archived_at) and is_nil(&1.completed_at)))
   end
