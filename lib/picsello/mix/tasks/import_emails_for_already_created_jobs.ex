dd19e88c35f78cffd71b868af31d8e572c4a85b8
hrd-2 <hrd2@picsello.com>
2023-10-12 18:04:13 +0500

    prevent emails in worker for already created leads/jobs

--- lib/picsello/mix/tasks/import_emails_for_already_created_jobs.ex
+++ lib/picsello/mix/tasks/import_emails_for_already_created_jobs.ex
@@ -22,6 +22,7 @@ defmodule Mix.Tasks.ImportEmailForAlreadyCreatedJobs do
     |> Enum.map(fn org ->
       user = %User{organization_id: org.id}
 
+      # Leads emails insert
       user
       |> Job.for_user()
       |> Job.leads()
@@ -29,6 +30,7 @@ defmodule Mix.Tasks.ImportEmailForAlreadyCreatedJobs do
       |> filter_jobs()
       |> leads_emails_insert(org.id)
 
+      # Job Gallery Order emails insert
       user
       |> Job.for_user()
       |> Job.not_leads()
@@ -50,17 +52,43 @@ defmodule Mix.Tasks.ImportEmailForAlreadyCreatedJobs do
 
   defp jobs_emails_insert(jobs, org_id) do
     Enum.map(jobs, fn job ->
-      skip_states =
-        if is_skip_states?(job.payment_schedules),
-          do: [""],
-          else: [:balance_due, :offline_payment, :paid_full, :paid_offline_full]
+      skip_states = skip_job_states(job)
 
       insert_email_schedules_job(job.type, job.id, org_id, [:job], skip_states)
       galleries_emails(job.galleries)
     end)
   end
 
-  defp is_skip_states?(payment_schedules),
+  defp skip_job_states(job) do
+    skip_payment_states =
+      if is_payment_states_keep?(job.payment_schedules),
+        do: [""],
+        else: [:balance_due, :offline_payment, :paid_full, :paid_offline_full]
+
+    skip_booking_states =
+      if Enum.count(job.payment_schedules) > 0,
+        do: [:pays_retainer, :thanks_booking, :pays_retainer_offline],
+        else: [""]
+
+    skip_shoot_states = skipping_actions_for_shoots(job.shoots)
+
+    skip_booking_states ++ skip_payment_states ++ skip_shoot_states
+  end
+
+  defp skipping_actions_for_shoots(shoots) do
+    case {has_shoots_before_now?(shoots), has_shoots_before_48_hours?(shoots)} do
+      {true, true} -> [:before_shoot, :shoot_thanks]
+      {true, _} -> [:before_shoot]
+      _ -> [""]
+    end
+  end
+
+  defp has_shoots_before_now?(shoots), do: Enum.any?(shoots, &(&1.starts_at < Timex.now()))
+
+  defp has_shoots_before_48_hours?(shoots),
+    do: Enum.any?(shoots, &(&1.starts_at < Timex.shift(Timex.now(), days: 2)))
+
+  defp is_payment_states_keep?(payment_schedules),
     do:
       payment_schedules
       |> Enum.any?(&is_nil(&1.reminded_at))
@@ -69,8 +97,9 @@ defmodule Mix.Tasks.ImportEmailForAlreadyCreatedJobs do
     galleries
     |> Enum.filter(&(&1.status == :active and !Galleries.expired?(&1)))
     |> Enum.map(fn gallery ->
-      EmailAutomationSchedules.insert_gallery_order_emails(gallery, nil)
-      order_emails(gallery.orders)
+      if Enum.count(gallery.orders) > 0,
+        do: order_emails(gallery.orders),
+        else: EmailAutomationSchedules.insert_gallery_order_emails(gallery, nil)
     end)
   end
 
@@ -97,7 +126,7 @@ defmodule Mix.Tasks.ImportEmailForAlreadyCreatedJobs do
   end
 
   defp filter_jobs(query) do
-    from(j in query, preload: [:job_status, :payment_schedules, galleries: :orders])
+    from(j in query, preload: [:job_status, :shoots, :payment_schedules, galleries: :orders])
     |> Repo.all()
     |> Enum.filter(&(is_nil(&1.archived_at) and is_nil(&1.completed_at)))
   end
