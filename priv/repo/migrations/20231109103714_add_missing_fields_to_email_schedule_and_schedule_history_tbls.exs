ee66f4a2e90732efff29c6b154505ef497cc9197
hrd-2 <hrd2@picsello.com>
2023-11-13 14:26:40 +0500

    fixed stopped reason migration

--- priv/repo/migrations/20231109103714_add_missing_fields_to_email_schedule_and_schedule_history_tbls.exs
+++ priv/repo/migrations/20231109103714_add_missing_fields_to_email_schedule_and_schedule_history_tbls.exs
@@ -1,14 +1,35 @@
 defmodule Picsello.Repo.Migrations.AddMissingFieldsToEmailScheduleAndScheduleHistoryTbls do
   use Ecto.Migration
 
+  alias Picsello.{
+    EmailAutomation.EmailSchedule,
+    EmailAutomationSchedules
+  }
+
   def up do
     alter table(:email_schedules) do
       add(:stopped_reason, :string)
     end
+
+    alter table(:email_schedules_history) do
+      add(:stopped_reason, :string)
+    end
+
+    flush()
+    email_schedules_query = from(es in EmailSchedule, where: not is_nil(es.stopped_at))
+
+    EmailAutomationSchedules.delete_and_insert_schedules_by(
+      email_schedules_query,
+      :photographer_stopped
+    )
   end
 
   def down do
     alter table(:email_schedules) do
+      remove(:stopped_reason)
+    end
+
+    alter table(:email_schedules_history) do
       remove(:stopped_reason)
     end
   end
